c0b5fe8d0cf62c64ba8998dd1767fd210742c464
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-07 03:01:08 +0000

    Split into files

---
+++ Arius/Arius/EncryptedAriusContent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Arius
+{
+    /// <summary>
+    /// Een Arius file met manifest en chunks
+    /// </summary>
+    class EncryptedAriusContent
+    {
+        public static EncryptedAriusContent CreateEncryptedAriusContent(LocalContentFile lcf, bool dedup, string passphrase, DirectoryInfo root) //AriusManifestFile amf, params EncryptedAriusChunk)
+        {
+            //DirectoryInfo tempDir = new DirectoryInfo(Path.Combine(_fi.Directory.FullName, _fi.Name + ".arius"));
+            //tempDir.Create();
+
+            var eacs = lcf
+                .GetChunks(dedup)
+                .AsParallel()
+                    .WithDegreeOfParallelism(1)
+                .Select(ac => ac.AsEncryptedAriusChunk(passphrase))
+                .ToArray();
+
+            var eamf = lcf
+                .GetManifest(eacs)
+                .GetAriusManifestFile(lcf.AriusManifestFullName)
+                .AsEncryptedAriusManifestFile(passphrase);
+
+
+            return new EncryptedAriusContent(eamf, eacs);
+        }
+
+
+        public EncryptedAriusContent(EncryptedAriusManifestFile eamf, EncryptedAriusChunk[] eacs)
+        {
+            _eamf = eamf;
+            _eacs = eacs;
+        }
+        private readonly EncryptedAriusManifestFile _eamf;
+        private readonly EncryptedAriusChunk[] _eacs;
+
+        public void Upload()
+        {
+
+        }
+
+        public void Restore()
+        {
+            //var chunkFiles = chunks.Select(c => new FileStream(Path.Combine(clf.FullName, BitConverter.ToString(c.Hash)), FileMode.Open, FileAccess.Read));
+            //var concaten = new ConcatenatedStream(chunkFiles);
+
+            //var restorePath = Path.Combine(clf.FullName, "haha.exe");
+            //using var fff = File.Create(restorePath);
+            //concaten.CopyTo(fff);
+            //fff.Close();
+        }
+    }
+}
---
+++ Arius/Arius/LocalContentFile.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Arius
+{
+    /// <summary>
+    /// Het gewone bestand met content erin
+    /// </summary>
+    class LocalContentFile : IChunk
+    {
+        public LocalContentFile(DirectoryInfo root, FileInfo localContent)
+        {
+            _root = root;
+            _localContent = localContent;
+            _hash = new Lazy<string>(() => FileUtils.GetHash(_localContent.FullName));
+        }
+        private readonly DirectoryInfo _root;
+        private readonly FileInfo _localContent;
+
+        public EncryptedAriusContent AsAriusContentFile(bool dedup, string passphrase, DirectoryInfo root)
+        {
+            return EncryptedAriusContent.CreateEncryptedAriusContent(this, dedup, passphrase, root);
+        }
+
+        public IChunk[] GetChunks(bool dedup)
+        {
+            if (dedup)
+            {
+                throw new NotImplementedException();
+
+                //var sb = new StreamBreaker();
+
+                //using var fs = new FileStream(_fi.FullName, FileMode.Open, FileAccess.Read);
+                //var chunks = sb.GetChunks(fs, fs.Length, SHA256.Create()).ToImmutableArray();
+                //fs.Position = 0;
+
+                //DirectoryInfo tempDir = new DirectoryInfo(Path.Combine(_fi.Directory.FullName, _fi.Name + ".arius"));
+                //tempDir.Create();
+
+                //foreach (var chunk in chunks)
+                //{
+                //    var chunkFullName = Path.Combine(tempDir.FullName, BitConverter.ToString(chunk.Hash));
+
+                //    byte[] buff = new byte[chunk.Length];
+                //    fs.Read(buff, 0, (int)chunk.Length);
+
+                //    using var fileStream = File.Create(chunkFullName);
+                //    fileStream.Write(buff, 0, (int)chunk.Length);
+                //    fileStream.Close();
+                //}
+
+                //fs.Close();
+
+                //var laf = new LocalAriusManifest(this);
+                //var lac = chunks.Select(c => new LocalAriusChunk("")).ToImmutableArray();
+
+                //var r = new AriusFile(this, laf, lac);
+
+                //return r;
+            }
+            else
+            {
+                return new IChunk[] { this }; // (_localContent, this.Hash) };
+            }
+        }
+
+        public AriusManifest GetManifest(params EncryptedAriusChunk[] chunks) => AriusManifest.CreateManifest(this, chunks);
+
+        //private AriusManifest CreateManifest(params EncryptedAriusChunk[] chunks) => AriusManifest.CreateManifest(this, chunks);
+
+        public string Hash => _hash.Value;
+        private Lazy<string> _hash;
+
+        public string FullName => _localContent.FullName;
+        public string RelativeName => Path.GetRelativePath(_root.FullName, FullName);
+        public string AriusManifestFullName => $"{FullName}.manifest.arius";
+        public DateTime CreationTimeUtc => _localContent.CreationTimeUtc;
+        public DateTime LastWriteTimeUtc => _localContent.LastWriteTimeUtc;
+    }
+}
