190a0bd546f439ebb6340cd917893710f3430ab2
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-12 15:05:12 +0100

    Code cleanup

---
+++ Arius/Arius/IUnencryptedChunk.cs
@@ -0,0 +1,15 @@
+﻿namespace Arius
+{
+    internal interface IUnencryptedChunk
+    {
+        string DirectoryName { get; }
+        string FullName { get; }
+
+        /// <summary>
+        /// The hash of the unencrytped chunk
+        /// </summary>
+        string Hash { get; }
+
+        EncryptedAriusChunk GetEncryptedAriusChunk(string passphrase);
+    }
+}
