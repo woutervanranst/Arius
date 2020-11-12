33f16bbb54825dcedbf2130e1d6f34b7168a28af
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-12 04:23:39 +0000

    Removed EncryptedAriusContent, reordered ArchiveCommand 1.1

--- Arius/Arius/EncryptedAriusContent.cs
+++ Arius/Arius/EncryptedAriusContent.cs
@@ -1,45 +1,45 @@
-﻿using System.Linq;
+﻿//using System.Linq;
 
-namespace Arius
-{
-    /// <summary>
-    /// Een Arius file met manifest en chunks //TODO Waarom bestaat deze class? volgens mij kan die weg - is gewoon een empty shell. de commands in CCreate draaien gewoon op lcf
-    /// </summary>
-    internal class EncryptedAriusContent
-    {
-        public static EncryptedAriusContent Create(LocalContentFile lcf, bool dedup, string passphrase, AriusRootDirectory root) //AriusManifestFile amf, params EncryptedAriusChunk)
-        {
-            var eacs = lcf
-                .GetChunks(dedup)
-                .AsParallel()
-                    .WithDegreeOfParallelism(1)
-                .Select(ac => ac.GetEncryptedAriusChunk(passphrase))
-                .ToArray();
+//namespace Arius
+//{
+//    /// <summary>
+//    /// Een Arius file met manifest en chunks //TODO Waarom bestaat deze class? volgens mij kan die weg - is gewoon een empty shell. de commands in CCreate draaien gewoon op lcf
+//    /// </summary>
+//    internal class EncryptedAriusContent
+//    {
+//        //public static EncryptedAriusContent Create(LocalContentFile lcf, bool dedup, string passphrase, AriusRootDirectory root) //AriusManifestFile amf, params EncryptedAriusChunk)
+//        {
+//            //var eacs = lcf
+//            //    .GetChunks(dedup)
+//            //    .AsParallel()
+//            //        .WithDegreeOfParallelism(1)
+//            //    .Select(ac => ac.GetEncryptedAriusChunk(passphrase))
+//            //    .ToArray();
 
-            var eamf = lcf
-                .GetManifest(eacs)
-                .CreateAriusManifestFile(lcf.AriusManifestFullName)
-                .CreateEncryptedAriusManifestFile(lcf.EncryptedAriusManifestFullName, passphrase, true);
+//            //var eamf = lcf
+//            //    .GetManifest(eacs)
+//            //    .CreateAriusManifestFile(lcf.AriusManifestFullName)
+//            //    .CreateEncryptedAriusManifestFile(lcf.EncryptedAriusManifestFullName, passphrase, true);
 
-            var p = eamf.CreatePointerFile(lcf); //TODO voor consistency de pointer hier buiten aanmaken / eg na de encryptedAriusContentsToUpload call?
+//            //var p = eamf.CreatePointerFile(lcf); //TODO voor consistency de pointer hier buiten aanmaken / eg na de encryptedAriusContentsToUpload call?
 
-            return new EncryptedAriusContent(lcf, eamf, eacs);
-        }
+//            //return new EncryptedAriusContent(lcf, eamf, eacs);
+//        }
 
 
-        public EncryptedAriusContent(LocalContentFile lcf, EncryptedAriusManifestFile encryptedManifest, EncryptedAriusChunk[] encryptedChunks)
-        {
-            LocalContentFile = lcf;
-            //PointerFile = pointerFile; //NOTE dit kunnnen er meerdere zijn
-            EncryptedManifestFile = encryptedManifest;
-            EncryptedAriusChunks = encryptedChunks;
-        }
+//        //public EncryptedAriusContent(LocalContentFile lcf, EncryptedAriusManifestFile encryptedManifest, EncryptedAriusChunk[] encryptedChunks)
+//        //{
+//        //    LocalContentFile = lcf;
+//        //    //PointerFile = pointerFile; //NOTE dit kunnnen er meerdere zijn
+//        //    EncryptedManifestFile = encryptedManifest;
+//        //    EncryptedAriusChunks = encryptedChunks;
+//        //}
 
-        public LocalContentFile LocalContentFile { get; }
-        //public AriusPointerFile PointerFile { get; } //NOTE dit kunnnen er meerdere zijn
-        public EncryptedAriusManifestFile EncryptedManifestFile { get; }
-        public EncryptedAriusChunk[] EncryptedAriusChunks { get; }
+//        //public LocalContentFile LocalContentFile { get; }
+//        ////public AriusPointerFile PointerFile { get; } //NOTE dit kunnnen er meerdere zijn
+//        //public EncryptedAriusManifestFile EncryptedManifestFile { get; }
+//        //public EncryptedAriusChunk[] EncryptedAriusChunks { get; }
 
-    }
+//    }
 
-}
+//}
