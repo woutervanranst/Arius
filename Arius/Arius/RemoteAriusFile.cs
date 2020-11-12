91df138f9f12992871a83f7392cd6d172188fc32
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-12 15:05:12 +0100

    Code cleanup

---
+++ Arius/Arius/RemoteAriusFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Azure.Storage.Blobs.Models;
+
+namespace Arius
+{
+    internal abstract class RemoteAriusFile
+    {
+        protected RemoteAriusFile(BlobItem bi)
+        {
+            if (!bi.Name.EndsWith(".arius"))
+                throw new ArgumentException("NOT A CHUNK"); //TODO
+
+            _bi = bi;
+        }
+        protected readonly BlobItem _bi;
+
+        public string Name => _bi.Name;
+        public abstract string Hash { get; }
+    }
+}
