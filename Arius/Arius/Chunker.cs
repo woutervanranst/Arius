7fae12ab3eeecffdea78109e51e790c12f2645f6
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-17 09:28:09 +0100

    Added Chunkers

---
+++ Arius/Arius/Chunker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arius.CommandLine;
+
+namespace Arius
+{
+    internal interface IChunkerOptions : ICommandExecutorOptions
+    {
+        bool Dedup { get; }
+    }
+    internal class Chunker : IChunker<LocalContentFile>
+    {
+        public IEnumerable<IChunk<LocalContentFile>> Chunk(LocalContentFile fileToChunk)
+        {
+            return new IChunk<LocalContentFile>[] { fileToChunk };
+        }
+
+        public LocalContentFile Merge(IEnumerable<IChunk<LocalContentFile>> chunksToJoin)
+        {
+            return (LocalContentFile)chunksToJoin.Single();
+        }
+    }
+
+    internal class DedupChunker : IChunker<LocalContentFile>
+    {
+        public IEnumerable<IChunk<LocalContentFile>> Chunk(LocalContentFile fileToChunk)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocalContentFile Merge(IEnumerable<IChunk<LocalContentFile>> chunksToJoin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
