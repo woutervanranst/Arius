495720f09010ef85e7da76103d5b63e3e6152429
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-17 06:53:38 +0100

    Working Local File hierarchy

--- Arius/Arius/Hasher.cs
+++ Arius/Arius/Hasher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,24 @@ using Arius.CommandLine;
 namespace Arius
 {
 
-    internal struct HashValue
+    internal struct HashValue // : IEquatable<HashValue>
     {
         public string Value { get; set; }
+
+        //public bool Equals(HashValue other)
+        //{
+        //    return Value == other.Value;
+        //}
+
+        //public override bool Equals(object obj)
+        //{
+        //    return obj is HashValue other && Equals(other);
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return (Value != null ? Value.GetHashCode() : 0);
+        //}
     }
 
     internal interface IHashValueProvider
@@ -39,12 +55,13 @@ namespace Arius
             return GetHashValue((dynamic) hashable);
         }
 
-        private HashValue GetHashValue<T>(IPointerFile<T> hashable) where T : IHashable
+        public HashValue GetHashValue(LocalPointerFile hashable)
         {
-            return hashable.GetObject().Hash;
+            var k = typeof(RemoteEncryptedManifestBlob).GetCustomAttribute<ExtensionAttribute>().Extension;
+            return new HashValue { Value = hashable.GetObjectName().TrimEnd(k) };
         }
 
-        private HashValue GetHashValue(ILocalContentFile hashable)
+        public HashValue GetHashValue(LocalContentFile hashable)
         {
             byte[] byteArray = Encoding.ASCII.GetBytes(_salt);
             using Stream ss = new MemoryStream(byteArray);
