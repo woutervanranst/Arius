a4c1b8e001f1330e369a2a5e83ca01908bce659b
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-20 06:18:13 +0100

    intermediate 2

--- Arius/Arius/Manifest.cs
+++ Arius/Arius/Manifest.cs
@@ -23,7 +23,7 @@ internal class Manifest //Marked as internal for Unit Testing
         Hash = hash;
     }
 
-    private static _pfeec = new PointerFileEntryEqualityComparer();
+    private static readonly PointerFileEntryEqualityComparer _pfeec = new PointerFileEntryEqualityComparer();
 
     // --- PROPERTIES
 
@@ -58,7 +58,7 @@ internal class Manifest //Marked as internal for Unit Testing
     /// Synchronize the state of the manifest to the current state of the file system:
     /// Additions, deletions, renames (= add + delete)
     /// </summary>
-    public bool Update(IEnumerable<IKaka> apfs)
+    public bool Update(IEnumerable<IPointersAndContent> apfs)
     {
         var fileSystemEntries = GetPointerFileEntries(apfs);
         var lastEntries = GetLastExistingEntriesPerRelativeName().ToImmutableArray();
@@ -83,11 +83,11 @@ internal class Manifest //Marked as internal for Unit Testing
     //}
 
     // --- RECORD DEFINITION & HELPERS
-    private static List<PointerFileEntry> GetPointerFileEntries(IEnumerable<IKaka> localContentFiles)
+    private static List<PointerFileEntry> GetPointerFileEntries(IEnumerable<IPointersAndContent> localContentFiles)
     {
         return localContentFiles.Select(lcf => GetPointerFileEntry(lcf)).ToList();
     }
-    private static PointerFileEntry GetPointerFileEntry(IKaka lcf)
+    private static PointerFileEntry GetPointerFileEntry(IPointersAndContent lcf)
     {
         return new PointerFileEntry(lcf.RelativeContentName, 
             DateTime.UtcNow, 
