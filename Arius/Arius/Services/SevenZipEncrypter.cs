0228f43e0fdc891a60f385d856faebd591ee0142
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-21 07:52:40 +0100

    Restructued files/folders

--- Arius/Arius/Services/SevenZipEncrypter.cs
+++ Arius/Arius/Services/SevenZipEncrypter.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Arius.CommandLine;
+using Arius.Extensions;
+using Arius.Models;
 using Microsoft.Extensions.Logging;
 using SevenZip;
 
-namespace Arius
+namespace Arius.Services
 {
     internal interface IEncrypterOptions : ICommandExecutorOptions
     {
