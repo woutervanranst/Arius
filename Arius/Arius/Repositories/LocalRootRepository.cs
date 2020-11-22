5b0ef41e9545716c081ad73fd68bfc67495f0bf5
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-22 06:09:31 +0100

    Refactired PointerService

--- Arius/Arius/Repositories/LocalRootRepository.cs
+++ Arius/Arius/Repositories/LocalRootRepository.cs
@@ -60,36 +60,4 @@ namespace Arius.Repositories
             manifestFiles.AsParallelWithParallelism().ForAll(Put);
         }
     }
-
-    internal class PointerService
-    {
-        private readonly LocalFileFactory _factory;
-
-        public PointerService(LocalFileFactory factory)
-        {
-            _factory = factory;
-        }
-        /// <summary>
-        /// Create a pointer for a local file with a remote manifest
-        /// </summary>
-        public IPointerFile CreatePointerFile(AriusRepository repository, ILocalContentFile lcf, IManifestFile manifestFile)
-        {
-            //TODO can be refactored to Put()?
-
-
-            var pointerFileInfo = lcf.PointerFileInfo;
-
-            if (pointerFileInfo.Exists)
-                throw new ArgumentException("The Pointer file already exists"); //TODO i  expect issies here when the binnary is changed?
-
-            File.WriteAllText(pointerFileInfo.FullName, manifestFile.Hash.Value);
-
-            pointerFileInfo.CreationTimeUtc = lcf.CreationTimeUtc;
-            pointerFileInfo.LastWriteTimeUtc = lcf.LastWriteTimeUtc;
-
-            return (LocalPointerFile) _factory.Create(pointerFileInfo, repository);
-        }
-
-
-    }
 }
