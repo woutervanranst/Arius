0a41eae3c93e958bd28f3dffcc8c4a19044f8474
Wouter Van Ranst <wouter.vanranst@gmail.com>
2020-11-22 05:22:15 +0100

    Progressbar tryout

---
+++ Arius/Arius/Extensions/ConsoleProgress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arius.Extensions
+{
+    internal class ConsoleProgress
+    {
+        private readonly long _max;
+        private long _current;
+
+        private readonly object _lock = new object();
+
+        public ConsoleProgress(long max)
+        {
+            _max = max;
+            _current = 0;
+        }
+
+        public void AddProgress(int i)
+        {
+            lock (_lock)
+            {
+                _current += i;
+                Console.Write($"\b\b\b\b{Math.Round(_current / ((float)_max) * 100)}%".PadLeft(4));
+
+            }
+        }
+    }
+}
