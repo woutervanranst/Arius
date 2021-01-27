007fcd210e3e76d30963cc5400436308d20edb55
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-01-27 06:38:26 +0100

    Update PointerService.cs

--- Arius/Arius/Services/PointerService.cs
+++ Arius/Arius/Services/PointerService.cs
@@ -56,12 +56,12 @@ namespace Arius.Services
 
                 File.WriteAllText(pointerFileInfo.FullName, manifestHash.Value);
 
-                File.SetCreationTimeUtc(pointerFileInfo.FullName, creationTimeUtc);
-                File.SetLastWriteTimeUtc(pointerFileInfo.FullName, lastWriteTimeUtc);
-
-                //FileInfo does not work according to https://stackoverflow.com/a/17126045/1582323
+                //FileInfo does not work on Linux according to https://stackoverflow.com/a/17126045/1582323
                 //pointerFileInfo.CreationTimeUtc = creationTimeUtc;
                 //pointerFileInfo.LastWriteTimeUtc = lastWriteTimeUtc;
+
+                File.SetCreationTimeUtc(pointerFileInfo.FullName, creationTimeUtc);
+                File.SetLastWriteTimeUtc(pointerFileInfo.FullName, lastWriteTimeUtc);
 
                 _logger.LogInformation($"Created PointerFile '{Path.GetRelativePath(root.FullName, pointerFileInfo.FullName)}'");
             }
