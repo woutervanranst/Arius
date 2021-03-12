6c80d3f927cf5d37883fdadda6a7d1b194c69d34
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-03-12 11:43:58 +0100

    Update PointerFileEntryRepository.cs
    cleanup

--- Arius/Arius/Repositories/PointerFileEntryRepository.cs
+++ Arius/Arius/Repositories/PointerFileEntryRepository.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using Arius.CommandLine;
 using Arius.Extensions;
 using Arius.Models;
-using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Extensions.Logging;
 
 namespace Arius.Repositories
@@ -67,8 +66,6 @@ namespace Arius.Repositories
                 if (await _repo.CreatePointerFileEntryIfNotExistsAsync(pfe))
                 {
                     //We inserted the entry
-                    //UpdateEntriesAtVersion(pfe);
-
                     if (pfe.IsDeleted)
                         _logger.LogInformation($"Deleted {pfe.RelativeName}");
                     else
@@ -95,7 +92,9 @@ namespace Arius.Repositories
             {
                 var version = await GetVersionAsync(pointInTime);
 
-                return await GetEntriesAtVersionAsync(version);
+                var r = await GetEntriesAtVersionAsync(version);
+
+                return r;
             }
 
             /// <summary>
@@ -106,6 +105,12 @@ namespace Arius.Repositories
             private async Task<DateTime> GetVersionAsync(DateTime pointInTime)
             {
                 var versions = (await GetVersionsAsync()).Reverse();
+
+                // if the pointInTime is a version - return the pointInTime
+                if (versions.Contains(pointInTime))
+                    return pointInTime;
+
+                // else, search for the version that exactly precedes the pointInTime
                 DateTime? version = null;
                 foreach (var v in versions)
                 {
@@ -134,40 +139,7 @@ namespace Arius.Repositories
                     .Select(z => z.OrderBy(pfe => pfe.Version).Last()).ToList();
 
                 return r;
-
-                //lock (entriesPerVersionLock)
-                //{
-                //    if (!entriesPerVersion.ContainsKey(version))
-                //    {
-                //        var entriesForThisVersion = pfes
-                //            .GroupBy(pfe => pfe.RelativeName)
-                //            .Select(g => g.Where(pfe => pfe.Version <= version)).Where(c => c.Any())
-                //            .Select(z => z.OrderBy(pfe => pfe.Version).Last()).ToList();
-
-                //        entriesPerVersion.Add(
-                //            version,
-                //            entriesForThisVersion);
-                //    }
-                //}
-
-                //return entriesPerVersion[version];
             }
-            //private readonly Dictionary<DateTime, List<PointerFileEntry>> entriesPerVersion = new();
-            //private readonly object entriesPerVersionLock = new object();
-
-            //private void UpdateEntriesAtVersion(PointerFileEntry pfe)
-            //{
-            //    lock (entriesPerVersionLock)
-            //    {
-            //        if (entriesPerVersion.ContainsKey(pfe.Version))
-            //            entriesPerVersion[pfe.Version].Add(pfe);
-            //        else
-            //            entriesPerVersion.Add(pfe.Version, new() { pfe });
-            //    }
-            //}
-
-            
-
 
             /// <summary>
             /// Returns an chronologically ordered list of versions (in universal time) for this repository
@@ -176,109 +148,6 @@ namespace Arius.Repositories
             public async Task<IEnumerable<DateTime>> GetVersionsAsync()
             {
                 return await _repo.GetVersionsAsync();
-            }
-
-
-
-            private List<PointerFileEntry> GetStateOn(DateTime pointInTime)
-            {
-                return null;
-
-
-                /* //TODO KARL is this optimal?
-                 * https://docs.microsoft.com/en-us/azure/cosmos-db/table-storage-design-guide#solution-6
-                 *  Table storage is lexicographically ordered ?
-                 */
-
-                //TODO karl multithreading debugging
-
-                //var zz = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>().AsEnumerable()
-                //    .Select(dto => CreatePointerFileEntry(dto)).ToArray();
-
-                //var zzzz = zz.Where(pfe => pfe.IsDeleted).ToArray();
-
-                //var zzz = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>().AsEnumerable()
-                //    .Select(dto => CreatePointerFileEntry(dto))
-                //    .GroupBy(pfe => pfe.RelativeName)
-                //    .Where(z => z.Count() > 1)
-                //    .ToArray();
-
-                //var zzzz = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>().AsEnumerable()
-                //    .Select(dto => CreatePointerFileEntry(dto))
-                //    .GroupBy(pfe => pfe.ManifestHash)
-                //    .Where(z => z.Count() > 1)
-                //    .ToArray();
-
-
-
-                //var r = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>()
-                //    .AsEnumerable()
-                //    .GroupBy(pfe => pfe.RelativeNameHash)   //more or less equiv as GroupBy(RelativeName) but the hash is tolower and platform neutral
-                //    .Select(g => g
-                //        .Where(dto => dto.Version <= pointInTime)
-                //        .OrderBy(dto => dto.Version).Last())
-                //    .Select(dto => CreatePointerFileEntry(dto));
-
-
-
-
-
-
-
-                //var a0 = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>()
-                //    .AsEnumerable().ToList();
-
-                //var a = a0
-                //    .GroupBy(pfe => pfe.RelativeNameHash).ToList();
-
-                //var b = a.Select(g => g
-                //        .Where(dto => dto.Version <= pointInTime)
-                //        .OrderBy(dto => dto.Version).Last());
-
-                //var c = b.Select(dto => CreatePointerFileEntry(dto));
-
-
-
-
-
-
-                ////////// get all rows - we're getting them anyway as GroupBy is not natively supported
-                ////////var r0 = _pointerEntryTable
-                ////////    .CreateQuery<PointerFileEntryDto>()
-                ////////    .AsEnumerable().ToArray();
-
-                ////////var r = r0
-                ////////    .Select(dto => CreatePointerFileEntry(dto))
-                ////////    .GroupBy(pfe => pfe.RelativeName)
-                ////////    .Select(g => g
-                ////////        .Where(pfe => pfe.Version <= pointInTime)
-                ////////        .OrderBy(pfe => pfe.Version).Last())
-                ////////    .ToList();
-
-                //var xxx = zz.Except(r).ToList();
-
-
-
-
-                //var r = _pointerEntryTable
-                //    .CreateQuery<PointerFileEntryDto>()
-                //    .AsEnumerable()
-                //    .GroupBy(pfe => pfe.RelativeNameHash)   //more or less equiv as GroupBy(RelativeName) but the hash is tolower and platform neutral
-                //    .Select(g => g
-                //        .Where(dto => dto.Version <= pointInTime)
-                //        .OrderBy(dto => dto.Version).Last())
-                //    .Select(dto => CreatePointerFileEntry(dto))
-                //    .ToDictionary(pfe => pfe.ManifestHash, pfe => new Dictionary<string, PointerFileEntry>()
-                //    {
-                //        {  pfe.RelativeName, pfe }
-                //    });
-
-                //return r;
             }
         }
 
