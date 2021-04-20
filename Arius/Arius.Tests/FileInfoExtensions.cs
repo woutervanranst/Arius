917254970ba7e2ba56640847f81b3036e7b3553c
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-04-20 20:53:28 +0200

    Update FileInfoExtensions.cs

--- Arius/Arius.Tests/FileInfoExtensions.cs
+++ Arius/Arius.Tests/FileInfoExtensions.cs
@@ -5,15 +5,25 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Arius.Extensions;
 using Arius.Models;
 
 namespace Arius.Tests
 {
     static class FileInfoExtensions
     {
-        public static FileInfo GetPointerFileInfo(this FileInfo localContentFileFileInfo)
+        public static PointerFile GetPointerFile(this FileInfo binaryFile)
         {
-            return new FileInfo(localContentFileFileInfo.FullName + PointerFile.Extension);
+            if (binaryFile.IsPointerFile()) throw new ArgumentException("this is not a BinaryFile");
+
+            return new PointerFile(binaryFile.Directory, binaryFile.GetPointerFileInfo());
+        }
+
+        public static FileInfo GetPointerFileInfo(this FileInfo binaryFile)
+        {
+            if (binaryFile.IsPointerFile()) throw new ArgumentException("this is not a BinaryFile");
+
+            return new FileInfo(binaryFile.FullName + PointerFile.Extension);
         }
     }
 }
