69c61f8f1a40f3902dc81f45282bfef2b27597fb
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-04-20 14:49:38 +0200

    Update DirectoryExtensions.cs

--- Arius/Arius/Extensions/DirectoryExtensions.cs
+++ Arius/Arius/Extensions/DirectoryExtensions.cs
@@ -39,25 +39,18 @@ namespace Arius.Extensions
             });
         }
 
-        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        public static void DirectoryCopy(this DirectoryInfo sourceDirectory, string destDirName, bool copySubDirs)
         {
-            // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            if (!sourceDirectory.Exists)
+                throw new DirectoryNotFoundException($"Source directory does not exist or could not be found: {sourceDirectory.FullName}");
 
-            if (!dir.Exists)
-            {
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + sourceDirName);
-            }
-
-            DirectoryInfo[] dirs = dir.GetDirectories();
+            var dirs = sourceDirectory.GetDirectories();
 
             // If the destination directory doesn't exist, create it.       
             Directory.CreateDirectory(destDirName);
 
             // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
+            FileInfo[] files = sourceDirectory.GetFiles();
             foreach (FileInfo file in files)
             {
                 string tempPath = Path.Combine(destDirName, file.Name);
@@ -70,7 +63,7 @@ namespace Arius.Extensions
                 foreach (DirectoryInfo subdir in dirs)
                 {
                     string tempPath = Path.Combine(destDirName, subdir.Name);
-                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                    DirectoryCopy(subdir, tempPath, copySubDirs);
                 }
             }
         }
