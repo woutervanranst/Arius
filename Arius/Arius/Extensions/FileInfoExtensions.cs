764773fb9b89224e63056173e8b879f7371b9485
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-04-20 14:49:32 +0200

    Create FileInfoExtensions.cs

---
+++ Arius/Arius/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Arius.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arius.Extensions
+{
+    static class FileInfoExtensions
+    {
+        public static bool IsPointerFile(this FileInfo fi) => fi.Name.EndsWith(PointerFile.Extension, StringComparison.CurrentCultureIgnoreCase);
+    }
+}
