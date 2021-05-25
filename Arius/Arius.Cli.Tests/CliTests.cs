c7e5c478d91ce4d56aff5ba9d0c6f99af29a329a
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-05-25 06:46:44 +0200

    first mocks

--- Arius/Arius.Cli.Tests/CliTests.cs
+++ Arius/Arius.Cli.Tests/CliTests.cs
@@ -14,11 +14,83 @@ namespace Arius.Tests.Arius.Cli
         [Test]
         public void Ha()
         {
-            var m = new Mock<Facade>();
+            var accountName = "ha";
+            var accountKey = "ha";
+            var container = "h";
+
+            var mfb = new Mock<Facade>();
+            var macb = new Mock<Facade.ArchiveCommandBuilder>();
+
+            macb.Setup(m => m.ForStorageAccount(accountName, accountKey)).Returns(macb.Object);
+            macb.Setup(m => m.ForContainer(container)).Returns(macb.Object);
+
+
+            mfb.Setup(m => m.GetArchiveCommandBuilder()).Returns(macb.Object);
+
+            var mf = mfb.Object;
+
+
+            var x = mf.GetArchiveCommandBuilder()
+                .ForStorageAccount(accountName, "h");
+
+            Assert.IsTrue(true);
+
+
+
+
+            //var myViewModel = TheOutletViewModelForTesting();
+            //var mockBuilder = new Mock<IOutletViewModelBuilder>();
+
+            //mockBuilder.Setup(m => m.WithOutlet(It.IsAny<int>())).Returns(mockBuilder.Object);
+            //mockBuilder.Setup(m => m.WithCountryList()).Returns(mockBuilder.Object);
+            //mockBuilder.Setup(m => m.Build()).Returns(myViewModel);
+
 
             //Facade f = m;
 
             //m.Setup(f => f.CreateArchiveCommand)
+
+
+            /*
+             * 
+             * 
+             * private async Task<IServiceProvider> ArchiveCommand(AccessTier tier, bool removeLocal = false, bool fastHash = false, bool dedup = false)
+        {
+            var cmd = "archive " +
+                $"-n {TestSetup.AccountName} " +
+                $"-k {TestSetup.AccountKey} " +
+                $"-p {TestSetup.passphrase} " +
+                $"-c {TestSetup.container.Name} " +
+                $"{(removeLocal ? "--remove-local " : "")}" +
+                $"--tier {tier.ToString().ToLower()} " +
+                $"{(dedup ? "--dedup " : "")}" +
+                $"{(fastHash ? "--fasthash" : "")}" +
+                $"{TestSetup.archiveTestDirectory.FullName}";
+
+            return await ExecuteCommand(cmd);   
+        }
+
+        private async Task<IServiceProvider> ExecuteCommand(string cmd)
+        {
+            Environment.SetEnvironmentVariable(Arius.AriusCommandService.CommandLineEnvironmentVariableName, cmd);
+
+            //Action<IConfigurationBuilder> bla = (b) =>
+            //{
+            //    b.AddInMemoryCollection(new Dictionary<string, string> {
+            //            { "TempDir:TempDirectoryName", ".ariustemp2" }
+            //        });
+            //};
+
+            await Arius.Program.CreateHostBuilder(cmd.Split(' '), null).RunConsoleAsync();
+
+            if (Environment.ExitCode != 0)
+                throw new ApplicationException("Exitcode is not 0");
+
+            var sp = TestSetup.GetServiceProvider();
+            return sp;
+        }
+             * 
+             */
         }
     }
 }
