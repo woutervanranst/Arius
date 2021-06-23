589392cbe4defe2378a8a524cf9a30a5cbcea3d0
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-06-23 06:01:01 +0200

    Added Left() helper method

--- Arius/Arius.Core/Extensions/StringExtensions.cs
+++ Arius/Arius.Core/Extensions/StringExtensions.cs
@@ -4,11 +4,17 @@ namespace Arius.Core.Extensions
 {
     internal static class StringExtensions
     {
-        // https://stackoverflow.com/questions/4101539/c-sharp-removing-substring-from-end-of-string
-
-
+        /// <summary>
+        /// Trim the given value from the end of the string
+        /// </summary>
+        /// <param name="inputText"></param>
+        /// <param name="value"></param>
+        /// <param name="comparisonType"></param>
+        /// <returns></returns>
         public static string TrimEnd(this string inputText, string value, StringComparison comparisonType = StringComparison.CurrentCultureIgnoreCase)
         {
+            // https://stackoverflow.com/questions/4101539/c-sharp-removing-substring-from-end-of-string
+
             if (!string.IsNullOrEmpty(value))
             {
                 while (!string.IsNullOrEmpty(inputText) && inputText.EndsWith(value, comparisonType))
@@ -18,6 +24,17 @@ namespace Arius.Core.Extensions
             }
 
             return inputText;
+        }
+
+
+        public static string Left(this string str, int length)
+        {
+            // https://stackoverflow.com/a/3566842/1582323
+
+            if (string.IsNullOrEmpty(str)) 
+                return str;
+
+            return str.Substring(0, Math.Min(str.Length, length));
         }
     }
 }
