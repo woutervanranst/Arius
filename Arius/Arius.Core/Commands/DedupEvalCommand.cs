55e58f0045da414be0c211ae68defd1ff7b0e969
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-07-02 18:00:52 +0200

    implemented dedupeval command

--- Arius/Arius.Core/Commands/DedupEvalCommand.cs
+++ Arius/Arius.Core/Commands/DedupEvalCommand.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Arius.Core.Extensions;
+using Arius.Core.Models;
+using Arius.Core.Services;
+using Arius.Core.Services.Chunkers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,9 +14,14 @@ using System.Threading.Tasks;
 namespace Arius.Core.Commands
 {
     internal class DedupEvalCommandOptions : Facade.Facade.IOptions,
-        DedupEvalCommand.IOptions
+        DedupEvalCommand.IOptions,
+
+        IHashValueProvider.IOptions
     {
         public DirectoryInfo Root { get; init; }
+
+        public string Passphrase => string.Empty; //No passphrase/hash seed needed
+        public bool FastHash => false; //Do not use fasthash
     }
 
     internal class DedupEvalCommand : ICommand //This class is internal but the interface is public for use in the Facade
@@ -23,22 +33,79 @@ namespace Arius.Core.Commands
 
         public DedupEvalCommand(IOptions options,
             ILogger<DedupEvalCommand> logger,
-            IServiceProvider serviceProvider)
+            IServiceProvider serviceProvider,
+            IHashValueProvider hvp,
+            ByteBoundaryChunker chunker)
         {
             this.options = options;
             this.logger = logger;
             this.services = serviceProvider;
+            this.hvp = hvp;
+            this.chunker = chunker;
         }
 
         private readonly IOptions options;
         private readonly ILogger<DedupEvalCommand> logger;
         private readonly IServiceProvider services;
+        private readonly IHashValueProvider hvp;
+        private readonly ByteBoundaryChunker chunker;
 
-        public Task<int> Execute()
+        public async Task<int> Execute()
         {
-            throw new NotImplementedException();
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+
+            var indexBlock = new IndexBlock(
+                logger: loggerFactory.CreateLogger<IndexBlock>(),
+                root: options.Root,
+                indexedFile: file => AddFile(file),
+                done: () => { });
+            var indexTask = indexBlock.GetTask;
+
+
+
+            await indexTask;
+
+            logger.LogInformation($"{fileCount} total files");
+            logger.LogInformation($"{uniqueFiles.Count} unique files");
+
+            logger.LogInformation($"{fileSize.GetBytesReadable()} total size");
+            logger.LogInformation($"{uniqueFiles.Values.Sum().GetBytesReadable()} size with file deduplication");
+            logger.LogInformation($"{uniqueChunks.Values.Sum().GetBytesReadable()} size with chunk deduplication");
+
+            return 0;
         }
 
         IServiceProvider ICommand.Services => throw new NotImplementedException();
+
+        private int fileCount;
+        private long fileSize;
+        private readonly Dictionary<HashValue, long> uniqueFiles = new();
+        private readonly Dictionary<HashValue, long> uniqueChunks = new();
+
+        private void AddFile(IFile f)
+        {
+            if (f is not BinaryFile)
+                return;
+
+            fileCount++;
+            fileSize += f.Length;
+
+
+            var bf = (BinaryFile)f;
+            bf.Hash = hvp.GetHashValue(bf);
+            if (!uniqueFiles.ContainsKey(bf.Hash))
+            { 
+                uniqueFiles.Add(bf.Hash, f.Length);
+
+                foreach (var c in chunker.Chunk(bf))
+                { 
+                    if (!uniqueChunks.ContainsKey(c.Hash))
+                        uniqueChunks.Add(c.Hash, c.Length);
+
+                    c.Delete();
+                }
+            }
+        }
     }
 }
