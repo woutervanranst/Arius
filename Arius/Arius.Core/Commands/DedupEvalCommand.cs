d096cab52cba20731040a411f335c18ec59fca37
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-07-02 17:07:40 +0200

    Create DedupEvalCommand.cs

---
+++ Arius/Arius.Core/Commands/DedupEvalCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arius.Core.Commands
+{
+    internal class DedupEvalCommand : ICommand //This class is internal but the interface is public for use in the Facade
+    {
+        internal class Options : Facade.Facade.IOptions
+        {
+            public DirectoryInfo Root { get; init; }
+        }
+
+        public DedupEvalCommand()
+        {
+
+        }
+
+        public Task<int> Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        IServiceProvider ICommand.Services => throw new NotImplementedException();
+    }
+}
