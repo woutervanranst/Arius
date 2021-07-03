0e5beff3f7b711b51e660fcc14cde3a60e99c7bf
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-07-03 10:34:47 +0200

    split off DedupEvalCommandOptions

--- Arius/Arius.Core/Commands/DedupEvalCommand.cs
+++ Arius/Arius.Core/Commands/DedupEvalCommand.cs
@@ -13,17 +13,6 @@ using System.Threading.Tasks;
 
 namespace Arius.Core.Commands
 {
-    internal class DedupEvalCommandOptions : Facade.Facade.IOptions,
-        DedupEvalCommand.IOptions,
-
-        IHashValueProvider.IOptions
-    {
-        public DirectoryInfo Root { get; init; }
-
-        public string Passphrase => string.Empty; //No passphrase/hash seed needed
-        public bool FastHash => false; //Do not use fasthash
-    }
-
     internal class DedupEvalCommand : ICommand //This class is internal but the interface is public for use in the Facade
     {
         internal interface IOptions
---
+++ Arius/Arius.Core/Commands/DedupEvalCommandOptions.cs
@@ -0,0 +1,16 @@
+﻿using Arius.Core.Services;
+using System.IO;
+
+namespace Arius.Core.Commands
+{
+    internal class DedupEvalCommandOptions : Facade.Facade.IOptions,
+        DedupEvalCommand.IOptions,
+
+        IHashValueProvider.IOptions
+    {
+        public DirectoryInfo Root { get; init; }
+
+        public string Passphrase => string.Empty; //No passphrase/hash seed needed
+        public bool FastHash => false; //Do not use fasthash
+    }
+}
