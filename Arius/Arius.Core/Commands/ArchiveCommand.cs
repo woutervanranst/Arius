764a839a8d4b9d927902ceb35efe95be5c2910fa
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-07-06 14:28:38 +0200

    moved namespaces

--- Arius/Arius.Core/Commands/ArchiveCommand.cs
+++ Arius/Arius.Core/Commands/ArchiveCommand.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Arius.Core.Services.Chunkers;
+using Arius.Core.Commands.Archive;
 
 namespace Arius.Core.Commands
 {
--- Arius/Arius.Core/Commands/ArchiveCommandBlocks.cs
+++ Arius/Arius.Core/Commands/ArchiveCommandBlocks.cs
@@ -18,7 +18,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Arius.Core.Commands
+namespace Arius.Core.Commands.Archive
 {
     internal class IndexBlock : TaskBlockBase<DirectoryInfo>
     {
