3fa56ef1be1816fbfab8d486a63e2841644ec24d
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-09-07 23:29:21 +0200

    Create CryptoTests.cs

---
+++ Arius/Arius.Core.Tests/UnitTests/CryptoTests.cs
@@ -0,0 +1,25 @@
+﻿using Arius.Core.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arius.Core.Tests.UnitTests
+{
+    class CryptoTests : TestBase
+    {
+        [Test]
+        public async Task Ha()
+        {
+            var original = "hahahahaha";
+            var passphrase = "mypassword";
+            var encrypted = CryptoService.Encrypt(original, passphrase); 
+
+            var decrypted = CryptoService.Decrypt(encrypted, passphrase);
+
+            Assert.AreEqual(original, decrypted);
+        }
+    }
+}
