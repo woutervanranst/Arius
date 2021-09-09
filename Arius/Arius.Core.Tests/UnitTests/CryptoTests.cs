2b87e65eb4ccbde33f7d872ace097c32c1fce8d9
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-09-09 13:29:56 +0200

    moved tests around

--- Arius/Arius.Core.Tests/UnitTests/CryptoTests.cs
+++ Arius/Arius.Core.Tests/UnitTests/CryptoTests.cs
@@ -11,7 +11,7 @@ namespace Arius.Core.Tests.UnitTests
     class CryptoTests : TestBase
     {
         [Test]
-        public async Task Ha()
+        public async Task EncryptAndDecrypt_String_Equal()
         {
             var original = "hahahahaha";
             var passphrase = "mypassword";
@@ -20,6 +20,89 @@ namespace Arius.Core.Tests.UnitTests
             var decrypted = CryptoService.Decrypt(encrypted, passphrase);
 
             Assert.AreEqual(original, decrypted);
+        }
+
+
+
+        [Test]
+        public async Task EncryptAndDecrypt_File_Equal()
+        {
+            var encFile = Path.GetTempFileName();
+            var decFile = Path.GetTempFileName();
+
+            const string passphrase = "testpw";
+
+            try
+            {
+                var sourceFile = EnsureArchiveTestDirectoryFileInfo();
+
+                using (var ss = File.OpenRead(sourceFile.FullName))
+                {
+                    using (var es = File.OpenWrite(encFile))
+                    {
+                        await CryptoService.CompressAndEncrypt(ss, es, passphrase);
+                    }
+                }
+
+                using (var es = File.OpenRead(encFile))
+                {
+                    using (var ts = File.OpenWrite(decFile))
+                    {
+                        await CryptoService.DecryptAndDecompress(es, ts, passphrase);
+                    }
+                }
+
+                var h1 = SHA256Hasher.GetHashValue(sourceFile.FullName, string.Empty);
+                var h2 = SHA256Hasher.GetHashValue(decFile, string.Empty);
+
+                Assert.AreEqual(h1, h2);
+            }
+            finally
+            {
+                File.Delete(encFile);
+                File.Delete(decFile);
+            }
+        }
+
+        [Test]
+        public async Task DecryptWithOpenSsl_File_Equal()
+        {
+            // Ensure compatibility with openssl
+
+            var openssl = ExternalProcess.FindFullName("openssl.exe", "openssl");
+            var gzip = ExternalProcess.FindFullName("gzip.exe", "gzip");
+
+            var encFile = Path.GetTempFileName();
+            var decFile = Path.GetTempFileName();
+
+            const string passphrase = "testpw";
+
+            try
+            {
+                var sourceFile = EnsureArchiveTestDirectoryFileInfo();
+
+                using (var ss = File.OpenRead(sourceFile.FullName))
+                {
+                    using (var es = File.OpenWrite(encFile))
+                    {
+                        await CryptoService.CompressAndEncrypt(ss, es, passphrase);
+                    }
+                }
+
+                ExternalProcess.RunSimpleProcess(openssl, $"enc -d -aes-256-cbc -in {encFile} -out {decFile}.gz -pass pass:\"{passphrase}\" -pbkdf2");
+                ExternalProcess.RunSimpleProcess(gzip, $"-d \"{decFile}.gz\" -f"); //-f for overwrite
+
+                var h1 = SHA256Hasher.GetHashValue(sourceFile.FullName, string.Empty);
+                var h2 = SHA256Hasher.GetHashValue(decFile, string.Empty);
+
+                Assert.AreEqual(h1, h2);
+            }
+            finally
+            {
+                File.Delete(encFile);
+                File.Delete(decFile);
+                File.Delete($"{decFile}.gz");
+            }
         }
     }
 }
