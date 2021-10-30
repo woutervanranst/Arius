6eed36e9cc3e40585dee3615298ccf865d0c737c
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-10-30 12:32:37 +0200

    rename

---
+++ Arius/Arius.Core/Repositories/PointerFileEntryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Arius.Core.Models;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging;
+
+namespace Arius.Core.Repositories;
+
+internal partial class Repository
+{
+    internal class PointerFileEntryRepository : AppendOnlyRepository<PointerFileEntry>
+    {
+        public PointerFileEntryRepository(ILogger<PointerFileEntryRepository> logger, IOptions options, BlobContainerClient container)
+            : base(logger, options, container)
+        {
+
+        }
+
+    }
+}
