5c334484ca7722b5c1af10be0ed5a9f1a3b3ed39
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-11-16 10:51:31 +0100

    Update ConcatenatedStream.cs

--- Arius/Arius.Core/Extensions/ConcatenatedStream.cs
+++ Arius/Arius.Core/Extensions/ConcatenatedStream.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Arius.Core.Extensions;
 
@@ -8,7 +10,7 @@ internal class ConcatenatedStream : Stream
 {
     public ConcatenatedStream(IEnumerable<Stream> streams)
     {
-        this.streams = new Queue<Stream>(streams);
+        this.streams = new(streams);
     }
     private readonly Queue<Stream> streams;
 
@@ -36,22 +38,39 @@ internal class ConcatenatedStream : Stream
     }
 
     public override bool CanSeek => false;
-
     public override bool CanWrite => false;
-
-    public override void Flush() => throw new NotImplementedException();
-
     public override long Length => throw new NotImplementedException();
+    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override void Flush() => throw new NotImplementedException();
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+    public override void SetLength(long value) => throw new NotImplementedException();
+    public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+}
 
-    public override long Position
+internal class ConcatenatedAsyncStream : Stream
+{
+    public ConcatenatedAsyncStream(IEnumerable<Task<Stream>> streams)
     {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
+        this.streams = new(streams);
+    }
+    private readonly Queue<Task<Stream>> streams;
+
+    public override bool CanRead => true;
+
+    public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+
+    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+        return base.ReadAsync(buffer, offset, count, cancellationToken);
     }
 
+    public override bool CanSeek => false;
+    public override bool CanWrite => false;
+    public override long Length => throw new NotImplementedException();
+    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override void Flush() => throw new NotImplementedException();
     public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
-
     public override void SetLength(long value) => throw new NotImplementedException();
-
     public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
 }
