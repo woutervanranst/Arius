bdb45eec57f7f36a3d9d93a2e5770c7681701d54
Wouter Van Ranst <wouter.vanranst@gmail.com>
2021-11-16 10:52:35 +0100

    Update ConcatenatedStream.cs

--- Arius/Arius.Core/Extensions/ConcatenatedStream.cs
+++ Arius/Arius.Core/Extensions/ConcatenatedStream.cs
@@ -46,31 +46,3 @@ internal class ConcatenatedStream : Stream
     public override void SetLength(long value) => throw new NotImplementedException();
     public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
 }
-
-internal class ConcatenatedAsyncStream : Stream
-{
-    public ConcatenatedAsyncStream(IEnumerable<Task<Stream>> streams)
-    {
-        this.streams = new(streams);
-    }
-    private readonly Queue<Task<Stream>> streams;
-
-    public override bool CanRead => true;
-
-    public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
-
-    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-        return base.ReadAsync(buffer, offset, count, cancellationToken);
-    }
-
-    public override bool CanSeek => false;
-    public override bool CanWrite => false;
-    public override long Length => throw new NotImplementedException();
-    public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public override void Flush() => throw new NotImplementedException();
-    public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
-    public override void SetLength(long value) => throw new NotImplementedException();
-    public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
-}
