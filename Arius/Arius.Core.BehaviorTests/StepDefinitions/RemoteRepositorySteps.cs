2a87c51222ffc7ae3c9804c715f09b2eca49e967
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-09-14 12:35:10 +0000

    extracted testbase hierarchy

--- Arius/Arius.Core.BehaviorTests/StepDefinitions/RemoteRepositorySteps.cs
+++ Arius/Arius.Core.BehaviorTests/StepDefinitions/RemoteRepositorySteps.cs
@@ -22,37 +22,7 @@ namespace Arius.Core.BehaviorTests.StepDefinitions
 {
     public record RepositoryOptions(string AccountName, string AccountKey, string Container, string Passphrase) : IRepositoryOptions;
 
-    [Binding]
-    class TestBase
-    {
-        public TestBase(ScenarioContext sc)
-        {
-            scenarioContext = sc;
-        }
-
-        protected readonly ScenarioContext scenarioContext;
-    }
-
-    class LocalTestBase : TestBase
-    {
-        public LocalTestBase(ScenarioContext sc, Directories dirs) : base(sc)
-        {
-            directories = dirs;
-        }
-
-        protected readonly Directories directories;
-
-        protected (PointerFile pf, PointerFileEntry? pfe) GetPointerInfo(FileInfo fi)
-        {
-            var pf = scenarioContext.GetPointerService().GetPointerFile(fi);
-
-            var a_rn = Path.GetRelativePath(directories.ArchiveTestDirectory.FullName, fi.FullName);
-            var pfe = scenarioContext.GetRepository().PointerFileEntries.GetCurrentEntriesAsync(includeDeleted: true).Result.SingleOrDefault(r => r.RelativeName.StartsWith(a_rn));
-
-            return (pf, pfe);
-
-        }
-    }
+    
     
     [Binding]
     class RemoteRepositorySteps : TestBase
---
+++ Arius/Arius.Core.BehaviorTests/StepDefinitions/TestBase.cs
@@ -0,0 +1,36 @@
+﻿using Arius.Core.Models;
+
+namespace Arius.Core.BehaviorTests.StepDefinitions
+{
+    [Binding]
+    class TestBase
+    {
+        public TestBase(ScenarioContext sc)
+        {
+            scenarioContext = sc;
+        }
+
+        protected readonly ScenarioContext scenarioContext;
+    }
+
+    class LocalTestBase : TestBase
+    {
+        public LocalTestBase(ScenarioContext sc, Directories dirs) : base(sc)
+        {
+            directories = dirs;
+        }
+
+        protected readonly Directories directories;
+
+        protected (PointerFile pf, PointerFileEntry? pfe) GetPointerInfo(FileInfo fi)
+        {
+            var pf = scenarioContext.GetPointerService().GetPointerFile(fi);
+
+            var a_rn = Path.GetRelativePath(directories.ArchiveTestDirectory.FullName, fi.FullName);
+            var pfe = scenarioContext.GetRepository().PointerFileEntries.GetCurrentEntriesAsync(includeDeleted: true).Result.SingleOrDefault(r => r.RelativeName.StartsWith(a_rn));
+
+            return (pf, pfe);
+
+        }
+    }
+}
