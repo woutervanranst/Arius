503c9d9ddac278d09b3ab84f7b939c95697ffb2b
Wouter Van Ranst <wouter.vanranst@gmail.com>
2022-12-22 15:59:36 +0100

    cleanup

--- Arius/Arius.Cli/Utils/TypeResolver.cs
+++ Arius/Arius.Cli/Utils/TypeResolver.cs
@@ -15,20 +15,7 @@ public sealed class TypeResolver : ITypeResolver, IDisposable
     public object Resolve(Type type)
     {
         if (type == null)
-        {
             return null;
-        }
-
-        /* NOTE: as of 20/11/21 -- when receiving a System.MethodAccessException: 'Attempt by method 'Microsoft.Extensions.Logging.Configuration.LoggerProviderConfigurationFactory.GetConfiguration(System.Type)' to access method 'Microsoft.Extensions.Logging.ProviderAliasUtilities.GetAlias(System.Type)' failed.'
-         * This is due to a package version conflict between
-         *      Karambolo.Extensions.Logging.File:3.2.1
-         * and 
-         *      Microsoft.Extensions.Configuration.Json:6.0.0
-         *      Microsoft.Extensions.DependencyInjection:6.0.0
-         *      Microsoft.Extensions.Logging:6.0.0
-         *
-         * Use Microsoft.Extensions.Hosting:6.0.0 instead
-         */
 
         return provider.GetService(type);
     }
@@ -36,8 +23,6 @@ public sealed class TypeResolver : ITypeResolver, IDisposable
     public void Dispose()
     {
         if (provider is IDisposable disposable)
-        {
             disposable.Dispose();
-        }
     }
 }
