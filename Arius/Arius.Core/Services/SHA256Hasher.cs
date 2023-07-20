954eac37c2c0906d3c9b6417cd5c163acc148343
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-07-20 18:47:01 +0200

    feat: added async overloads to the SHA256Hasher

--- Arius/Arius.Core/Services/SHA256Hasher.cs
+++ Arius/Arius.Core/Services/SHA256Hasher.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Arius.Core.Commands;
 using Arius.Core.Extensions;
 using Arius.Core.Models;
@@ -12,16 +13,18 @@ namespace Arius.Core.Services;
 
 internal interface IHashValueProvider
 {
-    BinaryHash GetBinaryHash(FileInfo bfi);
+    BinaryHash       GetBinaryHash(FileInfo      bfi);
+    Task<BinaryHash> GetBinaryHashAsync(FileInfo bfi);
     BinaryHash GetBinaryHash(string binaryFileFullName);
-        
+    Task<BinaryHash> GetBinaryHashAsync(string binaryFileFullName);
+
     ChunkHash GetChunkHash(string fullName);
     ChunkHash GetChunkHash(byte[] buffer);
 
     bool IsValid(Hash h);
 }
 
-internal class SHA256Hasher : IHashValueProvider
+internal partial class SHA256Hasher : IHashValueProvider
 {
     public SHA256Hasher(ILogger<SHA256Hasher> logger, IRepositoryOptions options)
     {
@@ -32,8 +35,10 @@ internal class SHA256Hasher : IHashValueProvider
     private readonly string salt;
     private readonly ILogger<SHA256Hasher> logger;
 
-    public BinaryHash GetBinaryHash(FileInfo bfi) => GetBinaryHash(bfi.FullName);
-    public BinaryHash GetBinaryHash(string binaryFileFullName) => new(GetHashValue(binaryFileFullName, salt));
+    public BinaryHash GetBinaryHash(FileInfo bfi)                               => GetBinaryHash(bfi.FullName);
+    public async Task<BinaryHash> GetBinaryHashAsync(FileInfo bfi)              => await GetBinaryHashAsync(bfi.FullName);
+    public BinaryHash GetBinaryHash(string binaryFileFullName)                  => new(GetHashValue(binaryFileFullName, salt));
+    public async Task<BinaryHash> GetBinaryHashAsync(string binaryFileFullName) => new(await GetHashValueAsync(binaryFileFullName, salt));
 
     public ChunkHash GetChunkHash(string fullName) => new(GetHashValue(fullName, salt));
     public ChunkHash GetChunkHash(byte[] buffer) => new(GetHashValue(buffer, salt));
@@ -71,20 +76,29 @@ internal class SHA256Hasher : IHashValueProvider
         if (h.Value.Length != 64)
             return false;
 
-        return Regex.Match(h.Value, SHA265WORDPATTERN).Success;
+        return SHA265WordRegex().Match(h.Value).Success;
     }
 
-    private const string SHA265WORDPATTERN = "^[a-f0-9]{64}$"; //https://stackoverflow.com/a/6630280/1582323 with A-F removed since we do .ToLower() in BytesToString
+
+    [GeneratedRegex("^[a-f0-9]{64}$")]
+    private static partial Regex SHA265WordRegex(); //https://stackoverflow.com/a/6630280/1582323 with A-F removed since we do .ToLower() in BytesToString
 
 
     internal static string GetHashValue(string fullName, string salt)
     {
-        using var fs = File.OpenRead(fullName);
+        using var fs = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize:4096, useAsync: true);
 
         return GetHashValue(fs, salt);
     }
+    internal static async Task<string> GetHashValueAsync(string fullName, string salt)
+    {
+        await using var fs = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096, useAsync: true);
 
-    private static string GetHashValue(Stream stream, string salt)
+        return await GetHashValueAsync(fs, salt);
+    }
+
+
+    private static string GetHashValue(Stream stream, string salt) // todo remove?
     {
         var saltBytes = Encoding.ASCII.GetBytes(salt);
         using var saltStream = new MemoryStream(saltBytes);
@@ -96,7 +110,19 @@ internal class SHA256Hasher : IHashValueProvider
 
         return BytesToString(hash);
     }
+    private static async Task<string> GetHashValueAsync(Stream stream, string salt)
+    {
+        var saltBytes = Encoding.ASCII.GetBytes(salt);
+        using var saltStream = new MemoryStream(saltBytes);
 
+        await using var saltedStream = new ConcatenatedStream(new Stream[] { saltStream, stream });
+
+        var hash = await SHA256.HashDataAsync(saltedStream);
+
+        return BytesToString(hash);
+    }
+
+    
     private static string GetHashValue(byte[] bytes, string salt)
     {
         var saltBytes = Encoding.ASCII.GetBytes(salt);
@@ -106,9 +132,7 @@ internal class SHA256Hasher : IHashValueProvider
 
         using var saltedStream = new ConcatenatedStream(new Stream[] { saltStream, stream });
 
-        using var sha256 = SHA256.Create(); //not thread safe so create a new instance each time
-
-        var hash = sha256.ComputeHash(saltedStream);
+        var hash = SHA256.HashData(saltedStream);
 
         return BytesToString(hash);
     }
