70e5b02efe575cfb15b0434c4b52c0f5c4ceda81
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-07-25 14:06:26 +0200

    Update FileInfoExtensions.cs

--- Arius/Arius.Core/Extensions/FileInfoExtensions.cs
+++ Arius/Arius.Core/Extensions/FileInfoExtensions.cs
@@ -7,11 +7,4 @@ namespace Arius.Core.Extensions;
 public static class FileInfoExtensions
 {
     internal static bool IsPointerFile(this FileInfo fi) => fi.Name.EndsWith(PointerFile.Extension, StringComparison.CurrentCultureIgnoreCase);
-
-    public static FileInfo GetPointerFileInfoFromBinaryFile(this FileInfo binaryFileInfo)
-    {
-        if (binaryFileInfo.IsPointerFile()) throw new ArgumentException("this is not a BinaryFile");
-
-        return new FileInfo(binaryFileInfo.FullName + PointerFile.Extension);
-    }
 }
