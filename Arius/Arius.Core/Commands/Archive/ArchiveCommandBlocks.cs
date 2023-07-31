441b6c6f4fa6f11420019a64484e6e5d60c6c58e
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-07-31 13:22:14 +0200

    chore: some cleanup of deprecated assets

--- Arius/Arius.Core/Commands/Archive/ArchiveCommandBlocks.cs
+++ Arius/Arius.Core/Commands/Archive/ArchiveCommandBlocks.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
-using Arius.Core.Extensions;
 using Arius.Core.Models;
 using Arius.Core.Repositories;
 using Arius.Core.Services;
--- Arius/Arius.Core/Services/SHA256Hasher.cs
+++ Arius/Arius.Core/Services/SHA256Hasher.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
