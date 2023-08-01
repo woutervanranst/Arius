8e6b73da087f1c4b9175cab45735d8a51c79ebbd
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-08-01 10:03:04 +0200

    Update Chunker.cs

--- Arius/Arius.Core/Services/Chunkers/Chunker.cs
+++ Arius/Arius.Core/Services/Chunkers/Chunker.cs
@@ -14,22 +14,4 @@ internal abstract class Chunker
     protected readonly IHashValueProvider hashValueProvider;
 
     public abstract IEnumerable<IChunk> Chunk(Stream streamToChunk);
-
-    //public async Task<BinaryFile> MergeAsync(DirectoryInfo root, IChunk[] chunks, FileInfo target)
-    //{
-    //    if (chunks.Length == 0)
-    //        throw new ArgumentException("No chunks to merge", nameof(chunks));
-
-    //    var chs = await Task.WhenAll(chunks.Select(async chunk => await chunk.OpenReadAsync()));
-    //    var stream = new ConcatenatedStream(chs);
-
-    //    using (var targetStream = target.Create())
-    //    {
-    //        await stream.CopyToAsync(targetStream);
-    //    }
-
-    //    var h = hashValueProvider.GetBinaryHash(target.FullName);
-
-    //    return new BinaryFile(root, target, h);
-    //}
 }
