33801d3ee8868993711e59e8e1e0a9efbf4ece58
Wouter Van Ranst <wouter.vanranst@gmail.com>
2023-08-23 14:03:55 +0200

    Update FileService.cs

--- src/Arius.UI/FileService.cs
+++ src/Arius.UI/FileService.cs
@@ -1,80 +1,35 @@
 ﻿using System.IO;
-using WouterVanRanst.Utils.Extensions;
 
 namespace Arius.UI;
 
 class FileService
 {
-    //public static async IAsyncEnumerable<(string RelativeParentPath, string DirectoryName, string Name)> GetEntriesAsync(
-    //    DirectoryInfo rootDir,
-    //    string? relativeParentPathEquals = null,
-    //    string? directoryNameEquals = null,
-    //    string? nameContains = null)
-    //{
-    //    if (relativeParentPathEquals is not null)
-    //        rootDir = rootDir.GetSubDirectory(relativeParentPathEquals);
-    //    if (directoryNameEquals is not null)
-    //        rootDir.GetSubDirectory(directoryNameEquals);
-
-    //    foreach (var d in rootDir.EnumerateFiles("*.pointer.arius"))
-    //        yield return (relativeParentPathEquals, GetDirectoryName(rootDir, rootDir), d.Name);
-
-    //    foreach (var dir2 in rootDir.EnumerateDirectories())
-    //    {
-    //        foreach (var d in dir2.EnumerateFiles("*.pointer.arius"))
-    //            yield return (relativeParentPathEquals, GetDirectoryName(rootDir, dir2), d.Name);
-    //    }
-
-    //    static string GetDirectoryName(DirectoryInfo root, DirectoryInfo dir)
-    //    {
-    //        if (root == dir)
-    //            return string.Empty;
-    //        else
-    //            return dir.Name;
-    //    }
-    //}
-
+    /// <summary>
+    /// Returns file entries from a given directory, and from its direct child directories.
+    /// </summary>
     public static async IAsyncEnumerable<(string RelativeParentPath, string DirectoryName, string Name)> GetEntriesAsync(
         DirectoryInfo rootDir,
         string? relativeParentPathEquals = null)
     {
         // If no relativeParentPathEquals is provided, return files from the root directory
         if (string.IsNullOrEmpty(relativeParentPathEquals))
-        {
             foreach (var file in rootDir.GetFiles())
-            {
                 yield return ("", "", file.Name);
-            }
-            //yield break;
-        }
 
         // Convert relative path to system-specific directory path
-        string adjustedRelativePath = relativeParentPathEquals.Replace('/', Path.DirectorySeparatorChar);
-        var    targetDir            = new DirectoryInfo(Path.Combine(rootDir.FullName, adjustedRelativePath));
-
-        // Ensure the target directory exists before proceeding
-        if (!targetDir.Exists) yield break;
-
-        string   directoryRelativeToRoot = targetDir.FullName.Substring(rootDir.FullName.Length).TrimStart(Path.DirectorySeparatorChar);
-        string[] pathSegments            = directoryRelativeToRoot.Split(Path.DirectorySeparatorChar);
-
-        // Deduce the DirectoryName and RelativeParentPath from the pathSegments
-        string directoryName      = pathSegments.Length > 0 ? pathSegments[pathSegments.Length - 1] : "";
-        string relativeParentPath = pathSegments.Length > 1 ? string.Join(Path.DirectorySeparatorChar, pathSegments, 0, pathSegments.Length - 1) : "";
-
-        //// Return files in the target directory
-        //foreach (var file in targetDir.EnumerateFiles())
-        //{
-        //    yield return (relativeParentPath, directoryName, file.Name);
-        //}
+        var adjustedRelativePath = relativeParentPathEquals.Replace('/', Path.DirectorySeparatorChar);
+        var targetDir            = new DirectoryInfo(Path.Combine(rootDir.FullName, adjustedRelativePath));
 
         // Return files from direct child directories
         foreach (var childDir in targetDir.EnumerateDirectories())
-        {
             foreach (var childFile in childDir.EnumerateFiles())
-            {
-                yield return (directoryRelativeToRoot, childDir.Name, childFile.Name);
-            }
+                yield return (GetRelativePath(rootDir.FullName, targetDir.FullName), childDir.Name, childFile.Name);
+
+
+        static string GetRelativePath(string relativeTo, string path)
+        {
+            var p = Path.GetRelativePath(relativeTo, path);
+            return p == "." ? "" : p;
         }
     }
 }
