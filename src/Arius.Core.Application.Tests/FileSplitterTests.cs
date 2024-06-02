a7af1c15f6df81c99fa574fa2212c7b7714f8931
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-06-02 15:33:26 +0200

    feat: added filesplitter

---
+++ src/Arius.Core.Application.Tests/FileSplitterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arius.Core.Application.Chunkers;
+
+namespace Arius.Core.Application.Tests;
+
+public class FileSplitterTests
+{
+    [Fact]
+    public void TestFileSplitter()
+    {
+        string inputFilePath   = @"C:\Users\WouterVanRanst\Downloads\FileZilla_3.67.0_win64-setup.exe";
+        string outputDirectory = @"C:\Users\WouterVanRanst\Downloads\Output";
+
+        Directory.CreateDirectory(outputDirectory);
+
+        var splitter   = new FileSplitter(inputFilePath, new byte[] { 0, 0 });
+        int partNumber = 0;
+
+        foreach (var part in splitter.Split())
+        {
+            string outputFilePath = Path.Combine(outputDirectory, $"part_{partNumber:D2}.bin");
+            File.WriteAllBytes(outputFilePath, part);
+            partNumber++;
+        }
+    }
+}
---
+++ src/Arius.Core.Application/Chunkers/FileSplitter.cs
@@ -0,0 +1,66 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace Arius.Core.Application.Chunkers;
+
+public class FileSplitter
+{
+    private readonly string _inputFilePath;
+    private readonly byte[] _pattern;
+
+    public FileSplitter(string inputFilePath, byte[] pattern)
+    {
+        _inputFilePath = inputFilePath;
+        _pattern       = pattern;
+    }
+
+    public IEnumerable<byte[]> Split()
+    {
+        using (var mmf = MemoryMappedFile.CreateFromFile(_inputFilePath, FileMode.Open))
+        {
+            using (var accessor = mmf.CreateViewAccessor())
+            {
+                long length       = accessor.Capacity;
+                long currentStart = 0;
+
+                for (long i = 0; i < length - _pattern.Length + 1; i++)
+                {
+                    if (MatchPattern(accessor, i, _pattern))
+                    {
+                        long partSize = i - currentStart;
+                        if (partSize > 0)
+                        {
+                            yield return ReadPart(accessor, currentStart, partSize);
+                            currentStart = i + _pattern.Length;
+                        }
+                        i += _pattern.Length - 1;
+                    }
+                }
+
+                // Yield the last part if any data remains
+                if (currentStart < length)
+                {
+                    yield return ReadPart(accessor, currentStart, length - currentStart);
+                }
+            }
+        }
+    }
+
+    private bool MatchPattern(MemoryMappedViewAccessor accessor, long position, byte[] pattern)
+    {
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (accessor.ReadByte(position + i) != pattern[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private byte[] ReadPart(MemoryMappedViewAccessor accessor, long start, long size)
+    {
+        byte[] buffer = new byte[size];
+        accessor.ReadArray(start, buffer, 0, buffer.Length);
+        return buffer;
+    }
+}
