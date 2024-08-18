8b584391d2aa066df6a74848eb30cc3b7d1c8cc7
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-08-18 08:26:25 +0200

    added ef migrations

---
+++ src/Arius.Web.Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Arius.Web.Infrastructure;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext> // used only for EF Core tools (e.g. dotnet ef migrations add ...)
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var basePath      = Directory.GetCurrentDirectory();
+        var directoryInfo = new DirectoryInfo(basePath);
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(directoryInfo.Parent.GetDirectories("Arius.Web").Single().FullName)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder          = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlite(connectionString);
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
