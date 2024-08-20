b846215bb3f988a590e9e7f82ab1ee9cdf89b0f3
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-08-20 06:49:54 +0200

    Create ContainerNamesQuery2.cs

---
+++ src/Arius.Core/Queries/ContainerNames2/ContainerNamesQuery2.cs
@@ -0,0 +1,37 @@
+﻿using Arius.Core.Facade;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arius.Core.Queries.ContainerNames2;
+
+public record ContainerNamesQuery2 : IRequest<IAsyncEnumerable<string>>, IStorageAccountOptions
+{
+    public required string AccountName { get; init; }
+    public required string AccountKey  { get; init; }
+    public required int    MaxRetries  { get; init; }
+}
+
+public class ContainerNamesQueryHandler : IRequestHandler<ContainerNamesQuery2, IAsyncEnumerable<string>>
+{
+    private readonly ILogger<ContainerNamesQueryHandler> logger;
+
+    public ContainerNamesQueryHandler(ILogger<ContainerNamesQueryHandler> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async Task<IAsyncEnumerable<string>> Handle(ContainerNamesQuery2 request, CancellationToken cancellationToken)
+    {
+        return GetContainerNamesAsync(cancellationToken);
+    }
+
+    private async IAsyncEnumerable<string> GetContainerNamesAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        yield return "a";
+        yield return "b";
+    }
+}
