d4b4428ab28f241bcc43fcd6d14c211112157e41
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-08-20 22:28:40 +0200

    added ef migration
    C:\Users\WouterVanRanst\Documents\GitHub\Arius\src>dotnet ef migrations add InitialCreate2 --project Arius.Web.Infrastructure

--- src/Arius.Web.Infrastructure/Migrations/20240820202820_InitialCreate2.Designer.cs
+++ src/Arius.Web.Infrastructure/Migrations/20240820202820_InitialCreate2.Designer.cs
@@ -2,6 +2,7 @@
 using Arius.Web.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Arius.Web.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240820202820_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.17");
---
+++ src/Arius.Web.Infrastructure/Migrations/20240820202820_InitialCreate2.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arius.Web.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BackupConfigurations");
+
+            migrationBuilder.CreateTable(
+                name: "StorageAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccountName = table.Column<string>(type: "TEXT", nullable: false),
+                    AccountKey = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StorageAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repositories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StorageAccountId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LocalPath = table.Column<string>(type: "TEXT", nullable: false),
+                    ContainerName = table.Column<string>(type: "TEXT", nullable: false),
+                    Passphrase = table.Column<string>(type: "TEXT", nullable: false),
+                    RemoveLocal = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Tier = table.Column<string>(type: "TEXT", nullable: false),
+                    Dedup = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FastHash = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repositories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Repositories_StorageAccounts_StorageAccountId",
+                        column: x => x.StorageAccountId,
+                        principalTable: "StorageAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repositories_StorageAccountId",
+                table: "Repositories",
+                column: "StorageAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Repositories");
+
+            migrationBuilder.DropTable(
+                name: "StorageAccounts");
+
+            migrationBuilder.CreateTable(
+                name: "BackupConfigurations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccountKey = table.Column<string>(type: "TEXT", nullable: false),
+                    AccountName = table.Column<string>(type: "TEXT", nullable: false),
+                    ContainerName = table.Column<string>(type: "TEXT", nullable: false),
+                    Dedup = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FastHash = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Passphrase = table.Column<string>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    RemoveLocal = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Tier = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BackupConfigurations", x => x.Id);
+                });
+        }
+    }
+}
