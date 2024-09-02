911692b808d27dadb097bb713f72be3e10486eea
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-09-02 11:42:33 +0200

    new db version

--- src/Arius.Web.Infrastructure/Migrations/20240902050309_InitialCreate3.Designer.cs
+++ src/Arius.Web.Infrastructure/Migrations/20240902050309_InitialCreate3.Designer.cs
@@ -2,6 +2,7 @@
 using Arius.Web.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Arius.Web.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240902050309_InitialCreate3")]
+    partial class InitialCreate3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
