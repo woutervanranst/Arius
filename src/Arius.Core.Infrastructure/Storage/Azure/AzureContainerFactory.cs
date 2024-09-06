867cf15d1d7c6ba43709bfc20ec3a6cf72fdaf81
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-09-06 04:50:53 +0200

    added create container

--- src/Arius.Core.Infrastructure/Storage/Azure/AzureContainerFactory.cs
+++ src/Arius.Core.Infrastructure/Storage/Azure/AzureContainerFactory.cs
@@ -1,19 +1,29 @@
-﻿using Arius.Core.Domain.Storage;
+﻿using System.Net;
+using Arius.Core.Domain.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Arius.Core.Infrastructure.Storage.Azure;
 
 public sealed class AzureContainerFactory
 {
-    private readonly AzureRepositoryFactory azureRepositoryFactory;
+    private readonly AzureRepositoryFactory         azureRepositoryFactory;
+    private readonly ILogger<AzureContainerFactory> logger;
 
-    public AzureContainerFactory(AzureRepositoryFactory azureRepositoryFactory)
+    public AzureContainerFactory(AzureRepositoryFactory azureRepositoryFactory, ILogger<AzureContainerFactory> logger)
     {
         this.azureRepositoryFactory = azureRepositoryFactory;
+        this.logger                 = logger;
     }
 
     internal IContainer Create(AzureStorageAccount storageAccount, BlobServiceClient blobServiceClient, string containerName)
     {
-        return new AzureContainer(storageAccount, blobServiceClient.GetBlobContainerClient(containerName), azureRepositoryFactory);
+        var bcc = blobServiceClient.GetBlobContainerClient(containerName);
+
+        var r = bcc.CreateIfNotExists(PublicAccessType.None);
+        if (r is not null && r.GetRawResponse().Status == (int)HttpStatusCode.Created)
+            logger.LogInformation("Container '{containerName}' created.", containerName);
+
+        return new AzureContainer(storageAccount, bcc, azureRepositoryFactory);
     }
 }
