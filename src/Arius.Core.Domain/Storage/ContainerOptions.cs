c9480bf4e23015f57511b6f22cd8b1d4ad5a31d1
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-09-10 14:21:53 +0200

    bugfix

--- src/Arius.Core.Domain/Storage/ContainerOptions.cs
+++ src/Arius.Core.Domain/Storage/ContainerOptions.cs
@@ -17,7 +17,7 @@ public class ContainerOptionsValidator : AbstractValidator<ContainerOptions>
         RuleFor(x => x.ContainerName)
             .NotEmpty()
             .WithMessage("Container name cannot be empty.")
-            .Matches(@"^[a-z0-9]{3,63}$") // Azure storage container names must be 3-63 characters long and can only contain lowercase letters, numbers, and hyphens
+            .Matches(@"^[a-z0-9-]{3,63}$") // Azure storage container names must be 3-63 characters long and can only contain lowercase letters, numbers, and hyphens
             .WithMessage("Container name must be between 3 and 63 characters and can only contain lowercase letters, numbers, and hyphens.");
 
     }
