3fef31f824721a15a4cb6aad042d74a89d1001d7
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-09-10 11:03:31 +0200

    bump nugets

--- src/Arius.Benchmarks/Sha256HasherBenchmarks.cs
+++ src/Arius.Benchmarks/Sha256HasherBenchmarks.cs
@@ -1,55 +1,55 @@
-﻿using Arius.Core.Domain.Storage.FileSystem;
-using Arius.Core.New.UnitTests.Extensions;
-using BenchmarkDotNet.Attributes;
-using WouterVanRanst.Utils.Extensions;
+﻿//using Arius.Core.Domain.Storage.FileSystem;
+//using Arius.Core.New.UnitTests.Extensions;
+//using BenchmarkDotNet.Attributes;
+//using WouterVanRanst.Utils.Extensions;
 
-namespace Arius.Benchmarks;
+//namespace Arius.Benchmarks;
 
-[MemoryDiagnoser]
-public class Sha256HasherBenchmarks
-{
-    private const string Salt = "woutervanranst";
-    private readonly string smallFile;
-    private readonly string largeFile;
-    private readonly Arius.Core.Services.SHA256Hasher arius3Hasher;
-    private readonly Arius.Core.Infrastructure.Services.SHA256Hasher arius4Hasher;
+//[MemoryDiagnoser]
+//public class Sha256HasherBenchmarks
+//{
+//    private const string Salt = "woutervanranst";
+//    private readonly string smallFile;
+//    private readonly string largeFile;
+//    private readonly Arius.Core.Services.SHA256Hasher arius3Hasher;
+//    private readonly Arius.Core.Infrastructure.Services.SHA256Hasher arius4Hasher;
 
-    public Sha256HasherBenchmarks()
-    {
-        // Setup files
-        smallFile = "smallTestFile.bin";
-        largeFile = "largeTestFile.bin";
+//    public Sha256HasherBenchmarks()
+//    {
+//        // Setup files
+//        smallFile = "smallTestFile.bin";
+//        largeFile = "largeTestFile.bin";
 
-        // Create test files
-        FileUtils.CreateRandomFile(smallFile, 1024 * 100); // 100 KB
-        FileUtils.CreateRandomFile(largeFile, 1024 * 1024 * 100); // 100 MB
+//        // Create test files
+//        FileUtils.CreateRandomFile(smallFile, 1024 * 100); // 100 KB
+//        FileUtils.CreateRandomFile(largeFile, 1024 * 1024 * 100); // 100 MB
 
-        // Initialize hashers
-        arius3Hasher = new(Salt);
-        arius4Hasher = new(Salt);
-    }
+//        // Initialize hashers
+//        arius3Hasher = new(Salt);
+//        arius4Hasher = new(Salt);
+//    }
 
-    [Benchmark]
-    public string HashSmallFileArius3()
-    {
-        return arius3Hasher.GetBinaryHash(smallFile).Value.BytesToHexString();
-    }
+//    [Benchmark]
+//    public string HashSmallFileArius3()
+//    {
+//        return arius3Hasher.GetBinaryHash(smallFile).Value.BytesToHexString();
+//    }
 
-    [Benchmark]
-    public async Task<string> HashSmallFileArius4()
-    {
-        return (await arius4Hasher.GetHashAsync(BinaryFile.FromFullName(null, smallFile))).Value.BytesToHexString();
-    }
+//    [Benchmark]
+//    public async Task<string> HashSmallFileArius4()
+//    {
+//        return (await arius4Hasher.GetHashAsync(BinaryFile.FromFullName(null, smallFile))).Value.BytesToHexString();
+//    }
 
-    [Benchmark]
-    public string HashLargeFileArius3()
-    {
-        return arius3Hasher.GetBinaryHash(largeFile).Value.BytesToHexString();
-    }
+//    [Benchmark]
+//    public string HashLargeFileArius3()
+//    {
+//        return arius3Hasher.GetBinaryHash(largeFile).Value.BytesToHexString();
+//    }
 
-    [Benchmark]
-    public async Task<string> HashLargeFileArius4()
-    {
-        return (await arius4Hasher.GetHashAsync(BinaryFile.FromFullName(null, largeFile))).Value.BytesToHexString();
-    }
-}
+//    [Benchmark]
+//    public async Task<string> HashLargeFileArius4()
+//    {
+//        return (await arius4Hasher.GetHashAsync(BinaryFile.FromFullName(null, largeFile))).Value.BytesToHexString();
+//    }
+//}
