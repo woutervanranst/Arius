663d262a4a51ac9588859d1ed8a65f951d5056e2
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-09-20 16:49:51 +0200

    adding stuff

---
+++ src/Arius.Blazor/FileProcessingNotificationHandler.cs
@@ -0,0 +1,114 @@
+using System.Collections.Concurrent;
+using Arius.Core.Domain.Storage.FileSystem;
+using Arius.Core.New.Commands.Archive;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Arius.Blazor;
+
+public sealed class ArchiveCommandNotificationHandler<T> 
+    : INotificationHandler<T> where T : ArchiveCommandNotification
+{
+    private readonly FileProcessingService s;
+
+    public ArchiveCommandNotificationHandler(FileProcessingService s)
+    {
+        this.s = s;
+    }
+
+    public async Task Handle(T notification, CancellationToken cancellationToken)
+    {
+        var result = notification switch
+        {
+            FilePairFoundNotification found             => s.HandleFilePairFoundNotification(found, cancellationToken),
+            FilePairHashingCompletedNotification hashed => s.HandleFilePairHashedNotification(hashed, cancellationToken),
+            ArchiveCommandDoneNotification done         => s.HandleArchiveCommandDoneNotification(done, cancellationToken),
+            _                                           => throw new InvalidOperationException("No event handler found for notification type")
+        };
+
+        await result;
+    }
+}
+
+public sealed class FileProcessingHub : Hub<IFileProcessingClient>
+{
+    private readonly FileProcessingService s;
+
+    public FileProcessingHub(FileProcessingService s)
+    {
+        this.s = s;
+    }
+
+    public async Task SendCurrentProgress()
+    {
+        // Get the current progress from the FileProcessingService
+        var currentFiles = s.GetCurrentFiles();
+
+        // Send the current progress to the caller
+        await Clients.Caller.FileFound(currentFiles.Count, currentFiles.Where(f => f.Value != "DONE").Select(f => f.Key.RelativeName).ToArray());
+
+        foreach (var file in currentFiles.Where(f => f.Value == "HASHED"))
+        {
+            await Clients.Caller.FileProcessed(file.Key.RelativeName);
+        }
+
+        // If all files are processed, signal that processing is completed
+        if (currentFiles.Values.All(status => status == "DONE"))
+        {
+            await Clients.Caller.FileProcessingCompleted();
+        }
+    }
+}
+
+public sealed class FileProcessingService
+{
+    private readonly IHubContext<FileProcessingHub, IFileProcessingClient> hubContext;
+
+    private readonly ConcurrentDictionary<IFilePair, string> files = new();
+
+    public FileProcessingService(IHubContext<FileProcessingHub, IFileProcessingClient> hubContext)
+    {
+        this.hubContext = hubContext;
+    }
+
+    public ConcurrentDictionary<IFilePair, string> GetCurrentFiles()
+    {
+        return files;
+    }
+
+    public Task HandleFilePairFoundNotification(FilePairFoundNotification notification, CancellationToken cancellationToken)
+    {
+        files.TryAdd(notification.FilePair, "FOUND");
+
+        hubContext.Clients.All.FileFound(files.Count,  files.Where(kvp => kvp.Value != "DONE").Select(kvp => kvp.Key.RelativeName).ToArray());
+
+        return Task.CompletedTask;
+    }
+
+    public Task HandleFilePairHashedNotification(FilePairHashingCompletedNotification notification, CancellationToken cancellationToken)
+    {
+        files[notification.FilePairWithHash] = "HASHED";
+        //files.TryUpdate(notification.FilePairWithHash, "HASHED", "FOUND");
+
+        //hubContext.Clients.All.FileProcessed()
+
+        return Task.CompletedTask;
+    }
+
+    public Task HandleArchiveCommandDoneNotification(ArchiveCommandDoneNotification notification, CancellationToken cancellationToken)
+    {
+        //throw new NotImplementedException();
+
+        hubContext.Clients.All.FileProcessingCompleted();
+
+        return Task.CompletedTask;
+    }
+}
+
+public interface IFileProcessingClient
+{
+    Task FileFound(int fileCounter, IReadOnlyCollection<string> recentFiles);
+    Task FileProcessed(string fileName);
+    Task FileProcessingCompleted();
+}
+
--- src/Arius.Blazor/Program.cs
+++ src/Arius.Blazor/Program.cs
@@ -1,47 +1,108 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Arius.Blazor.Data;
+using Arius.Blazor;
 using Arius.Blazor._keenthemes;
 using Arius.Blazor._keenthemes.libs;
+using Arius.Blazor.Data;
+using Arius.Core.New;
+using Arius.Web.Application;
+using Arius.Web.Domain;
+using Arius.Web.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Arius.Web;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<IKTTheme, KTTheme>();
-builder.Services.AddSingleton<IBootstrapBase, BootstrapBase>();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-var app = builder.Build();
-
-IConfiguration themeConfiguration = new ConfigurationBuilder()
-                            .AddJsonFile("_keenthemes/config/themesettings.json")
-                            .Build();
-
-IConfiguration iconsConfiguration = new ConfigurationBuilder()
-                            .AddJsonFile("_keenthemes/config/icons.json")
-                            .Build();
-
-KTThemeSettings.init(themeConfiguration);
-KTIconsSettings.init(iconsConfiguration);
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public sealed class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add SignalR
+        builder.Services.AddSignalR();
+
+        // Configure Services
+        ConfigureServices(builder.Services, builder.Configuration);
+
+        var app = builder.Build();
+
+        // Ensure the database is created and applies any pending migrations
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.Migrate();
+        }
+
+        // Configure KTTheme and Icons settings
+        IConfiguration themeConfiguration = new ConfigurationBuilder()
+                                .AddJsonFile("_keenthemes/config/themesettings.json")
+                                .Build();
+
+        IConfiguration iconsConfiguration = new ConfigurationBuilder()
+                                .AddJsonFile("_keenthemes/config/icons.json")
+                                .Build();
+
+        KTThemeSettings.init(themeConfiguration);
+        KTIconsSettings.init(iconsConfiguration);
+
+        app.MapHub<FileProcessingHub>("/jobsHub");
+
+        // Configure the HTTP request pipeline
+        ConfigureMiddleware(app);
+
+        app.Run();
+    }
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        // Add Razor Components services with Interactive Server Components
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        // Add SyncFusion
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cXGNCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXdccXRWRmBZUk10WkE=");
+        services.AddSyncfusionBlazor();
+
+        // Configure DbContext with connection string
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+        // Register application services
+        services.AddScoped<IStorageAccountRepository, StorageAccountRepository>();
+        services.AddScoped<IRepositoryRepository, RepositoryRepository>();
+        services.AddScoped<RepositoryService>();
+        services.AddSingleton<FileProcessingService>();
+
+        services.AddArius(c => c.LocalConfigRoot = new DirectoryInfo(configuration.GetValue<string>("LocalConfigRoot")));
+
+        // Add theme services
+        services.AddSingleton<WeatherForecastService>();
+        services.AddSingleton<IKTTheme, KTTheme>();
+        services.AddSingleton<IBootstrapBase, BootstrapBase>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    }
+
+    private static void ConfigureMiddleware(WebApplication app)
+    {
+        // Configure exception handling and HSTS
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        // Configure remaining middleware
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        // Map Razor Components with Interactive Server Render Mode
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.UseRouting();
+
+        // Map Blazor Hub
+        app.MapBlazorHub();
+        //app.MapFallbackToPage("/_Host");
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
