dd37003252a5c4d6afa0fb36dfb0a83d02846060
Wouter Van Ranst <wouter.vanranst@gmail.com>
2024-10-09 08:24:12 +0200

    chore: rename Arius.Core to Arius.Core.Old

--- src/Arius.Core.New/InternalsVisibleTo.cs
+++ src/Arius.Core.New/InternalsVisibleTo.cs
@@ -3,5 +3,5 @@
 /*
  * This is required to test the internals of the Arius.Core assembly
  */
-[assembly: InternalsVisibleTo("Arius.Core.New.UnitTests")]
+[assembly: InternalsVisibleTo("Arius.Core.Tests")]
 [assembly: InternalsVisibleTo("Arius.ArchUnit")]
