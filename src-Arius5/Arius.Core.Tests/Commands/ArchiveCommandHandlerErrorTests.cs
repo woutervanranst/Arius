658775cf483126307e2975d0e89fcd1c833443f3
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-06-03 13:58:34 +0200

    Create ArchiveCommandHandlerErrorTests.cs

---
+++ src-Arius5/Arius.Core.Tests/Commands/ArchiveCommandHandlerErrorTests.cs
@@ -0,0 +1,57 @@
+using Arius.Core.Commands;
+using Arius.Core.Models;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Testing;
+using NSubstitute;
+
+namespace Arius.Core.Tests.Commands;
+
+public class ArchiveCommandHandlerErrorTests : IDisposable
+{
+    private readonly FakeLogger<ArchiveCommandHandler> logger;
+    private readonly ArchiveCommandHandler             handler;
+    private readonly Fixture                           fixture;
+
+    public ArchiveCommandHandlerErrorTests()
+    {
+        logger  = new();
+        handler = new ArchiveCommandHandler(logger);
+        fixture = new ();
+    }
+
+    private ArchiveCommand CreateTestCommand()
+    {
+        return new ArchiveCommand
+        {
+            AccountName = "testaccount",
+            AccountKey = "testkey",
+            ContainerName = "testcontainer",
+            Passphrase = "testpassphrase",
+            RemoveLocal = false,
+            Tier = StorageTier.Cool,
+            LocalRoot = fixture.TestRunSourceFolder,
+            Parallelism = 1,
+            SmallFileBoundary = 2 * 1024 * 1024
+        };
+    }
+
+    [Fact]
+    public async Task Handle_WithInvalidAzureCredentials_ShouldFail()
+    {
+        // Arrange
+        var command = CreateTestCommand();
+        command = command with 
+        {
+            AccountName = "nonexistentaccount",
+            AccountKey = "invalid_key_that_will_cause_authentication_failure"
+        };
+
+        // Act
+        var act = () => handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        var e = await act.Should().ThrowAsync<FormatException>();
+        e.Which.Message.Should().Contain("No valid combination of account information found.");
+    }
+}
