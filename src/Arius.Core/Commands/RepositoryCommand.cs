9c86ffc61280e7bae0edcff0c95b8a39c7c532e4
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-08-31 11:53:53 +0200

    Update RepositoryCommand.cs

--- src/Arius.Core/Commands/RepositoryCommand.cs
+++ src/Arius.Core/Commands/RepositoryCommand.cs
@@ -7,5 +7,5 @@ public abstract record RepositoryCommand : ICommand
     public required string AccountName   { get; init; }
     public required string AccountKey    { get; init; }
     public required string ContainerName { get; init; }
-    public required string Passphrase    { get; init; }
+    public required string Passphrase    { get; init; } // TODO investigate SecureString
 }
