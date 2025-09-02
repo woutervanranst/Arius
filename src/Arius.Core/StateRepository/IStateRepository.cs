84e7b67022669c3e52bf09cfef24dd741e640332
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-09-02 17:31:40 +0200

    chore: restructure StateRepository

---
+++ src/Arius.Core/StateRepository/IStateRepository.cs
@@ -0,0 +1,16 @@
+﻿using Arius.Core.Models;
+
+namespace Arius.Core.Repositories;
+
+internal interface IStateRepository
+{
+    FileInfo                         StateDatabaseFile { get; }
+    bool                             HasChanges        { get; }
+    void                             Vacuum();
+    void                             Delete();
+    BinaryPropertiesDto?             GetBinaryProperty(Hash h);
+    void                             AddBinaryProperties(params BinaryPropertiesDto[] bps);
+    void                             UpsertPointerFileEntries(params PointerFileEntryDto[] pfes);
+    IEnumerable<PointerFileEntryDto> GetPointerFileEntries(string relativeNamePrefix, bool includeBinaryProperties = false);
+    void                             DeletePointerFileEntries(Func<PointerFileEntryDto, bool> shouldBeDeleted);
+}
--- src/Arius.Core/StateRepository/StateRepository.cs
+++ src/Arius.Core/StateRepository/StateRepository.cs
@@ -5,19 +5,6 @@ using WouterVanRanst.Utils.Extensions;
 
 namespace Arius.Core.Repositories;
 
-internal interface IStateRepository
-{
-    FileInfo                         StateDatabaseFile { get; }
-    bool                             HasChanges        { get; }
-    void                             Vacuum();
-    void                             Delete();
-    BinaryPropertiesDto?             GetBinaryProperty(Hash h);
-    void                             AddBinaryProperties(params BinaryPropertiesDto[] bps);
-    void                             UpsertPointerFileEntries(params PointerFileEntryDto[] pfes);
-    IEnumerable<PointerFileEntryDto> GetPointerFileEntries(string relativeNamePrefix, bool includeBinaryProperties = false);
-    void                             DeletePointerFileEntries(Func<PointerFileEntryDto, bool> shouldBeDeleted);
-}
-
 internal class StateRepository : IStateRepository
 {
     private readonly StateRepositoryDbContextFactory factory;
