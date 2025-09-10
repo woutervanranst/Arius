d3822a031b7aedea2b925fc1e074147effea9038
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-09-10 21:20:48 +0200

    feat: implement cli live updates

--- src/Arius.Cli/CliCommands/RestoreCliCommand.cs
+++ src/Arius.Cli/CliCommands/RestoreCliCommand.cs
@@ -4,8 +4,11 @@ using CliFx.Exceptions;
 using CliFx.Infrastructure;
 using Mediator;
 using Microsoft.Extensions.Logging;
+using Spectre.Console;
 using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Arius.Cli.CliCommands;
@@ -48,35 +51,138 @@ public abstract class RestoreCliCommandBase : CliFx.ICommand
     {
         try
         {
-            var command = new RestoreCommand
-            {
-                AccountName     = AccountName,
-                AccountKey      = AccountKey,
-                ContainerName   = ContainerName,
-                Passphrase      = Passphrase,
-                LocalRoot       = LocalRoot,
-                Targets         = Targets,
-                Download        = Download,
-                IncludePointers = IncludePointers,
-                //ProgressReporter = pu
-            };
+            AnsiConsole.Write(
+                new FigletText("Arius")
+                    .LeftJustified()
+                    .Color(Color.Red));
 
-            var cancellationToken = console.RegisterCancellationHandler();
-            var result = await mediator.Send(command, cancellationToken);
+            await AnsiConsole.Progress()
+                .AutoRefresh(true)
+                .AutoClear(false)
+                .HideCompleted(true)
+                .Columns(
+                    new ElapsedTimeColumn(),
+                    new ProgressBarColumn(),
+                    new TaskDescriptionColumn { Alignment = Justify.Right })
+                .StartAsync(async ctx =>
+                {
+                    var progressUpdates = new ConcurrentQueue<ProgressUpdate>();
+
+                    // Create the Mediator command from the CLI arguments
+                    var command = new RestoreCommand
+                    {
+                        AccountName     = AccountName,
+                        AccountKey      = AccountKey,
+                        ContainerName   = ContainerName,
+                        Passphrase      = Passphrase,
+                        LocalRoot       = LocalRoot,
+                        Targets         = Targets,
+                        Download        = Download,
+                        IncludePointers = IncludePointers,
+                        ProgressReporter = new Progress<ProgressUpdate>(u => progressUpdates.Enqueue(u))
+                    };
+
+                    var mainTask = ctx.AddTask("[cyan1]Initializing restore...[/]");
+                    var fileTask = ctx.AddTask("[cyan3]Files[/]", false);
+
+                    // Start the restore command in the background
+                    var cancellationToken = console.RegisterCancellationHandler();
+                    var commandTask       = mediator.Send(command, cancellationToken);
+
+                    var taskDictionary = new ConcurrentDictionary<string, ProgressTask>();
+
+                    // Process progress updates
+                    var lastRehydrationCount = 0;
+                    while (!commandTask.IsCompleted)
+                    {
+                        while (progressUpdates.TryDequeue(out var update))
+                        {
+                            switch (update)
+                            {
+                                case TaskProgressUpdate taskUpdate:
+                                    mainTask.Value = taskUpdate.Percentage;
+                                    mainTask.Description = taskUpdate.StatusMessage ?? $"[cyan1]{TruncateLeft(taskUpdate.TaskName, 50)}[/]";
+                                    break;
+
+                                case FileProgressUpdate fileUpdate:
+                                    if (!fileTask.IsStarted)
+                                        fileTask.StartTask();
+                                    fileTask.Value = fileUpdate.Percentage;
+                                    fileTask.Description = fileUpdate.StatusMessage ?? $"[cyan3]{TruncateLeft(fileUpdate.FileName, 50)}[/]";
+                                    break;
+
+                                case RehydrationProgressUpdate rehydrationUpdate:
+                                    if (rehydrationUpdate.Count != lastRehydrationCount)
+                                    {
+                                        lastRehydrationCount = rehydrationUpdate.Count;
+                                        mainTask.Description = $"[yellow]Rehydrating blobs ({lastRehydrationCount})... {rehydrationUpdate.Status}[/]";
+                                    }
+                                    break;
+                            }
+                        }
+
+                        await Task.Delay(50, cancellationToken);
+                    }
+
+                    // Process any remaining updates
+                    while (progressUpdates.TryDequeue(out var update))
+                    {
+                        switch (update)
+                        {
+                            case TaskProgressUpdate taskUpdate:
+                                mainTask.Value = taskUpdate.Percentage;
+                                mainTask.Description = taskUpdate.StatusMessage ?? $"[cyan1]{TruncateLeft(taskUpdate.TaskName, 50)}[/]";
+                                break;
+
+                            case FileProgressUpdate fileUpdate:
+                                if (!fileTask.IsStarted)
+                                    fileTask.StartTask();
+                                fileTask.Value = fileUpdate.Percentage;
+                                fileTask.Description = fileUpdate.StatusMessage ?? $"[cyan3]{TruncateLeft(fileUpdate.FileName, 50)}[/]";
+                                break;
+
+                            case RehydrationProgressUpdate rehydrationUpdate:
+                                if (rehydrationUpdate.Count != lastRehydrationCount)
+                                {
+                                    lastRehydrationCount = rehydrationUpdate.Count;
+                                    mainTask.Description = $"[yellow]Rehydrating blobs ({lastRehydrationCount})... {rehydrationUpdate.Status}[/]";
+                                }
+                                break;
+                        }
+                    }
+
+                    // Ensure tasks are completed
+                    mainTask.Value = 100;
+                    mainTask.Description = "[green]Restore completed[/]";
+                    if (fileTask.IsStarted)
+                    {
+                        fileTask.Value = 100;
+                        fileTask.Description = "[green]Files completed[/]";
+                    }
+
+                    // Wait for the actual command to complete and get result
+                    await commandTask;
+                });
         }
         catch (Exception e)
         {
             logger.LogError(e, "Unhandled exception");
             throw new CommandException(e.Message, showHelp: false, innerException: e);
         }
-        //catch (ValidationException e)
-        //{
-        //    throw new CommandException(e.Message, showHelp: true);
-        //}
-        //catch (Exception e)
-        //{
-        //    AnsiConsole.WriteException(e, ExceptionFormats.ShortenEverything);
-        //}
+    }
+
+    private static string TruncateLeft(string text, int maxLength)
+    {
+        if (text.Length <= maxLength)
+            return text;
+        return "..." + text.Substring(text.Length - maxLength + 3);
+    }
+
+    private static string TruncateRight(string text, int maxLength)
+    {
+        if (text.Length <= maxLength)
+            return text;
+        return text.Substring(0, maxLength - 3) + "...";
     }
 }
 
