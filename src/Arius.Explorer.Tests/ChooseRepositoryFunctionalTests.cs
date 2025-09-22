0d7fd0040297d87cd5fd6ab0d2a89afae6961d8f
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-09-22 08:43:14 +0200

    tgd

--- src/Arius.Explorer.Tests/ChooseRepositoryFunctionalTests.cs
+++ src/Arius.Explorer.Tests/ChooseRepositoryFunctionalTests.cs
@@ -34,7 +34,7 @@ public class ChooseRepositoryFunctionalTests : ExplorerUiTestBase
     }
 
     [Test]
-    [Ignore("Needs rework")]
+    //[Ignore("Needs rework")]
     public void ChooseRepository_ShouldDisplayDefaultValues()
     {
         var dialog = _dialog.ShouldNotBeNull("Choose Repository dialog was not opened.");
