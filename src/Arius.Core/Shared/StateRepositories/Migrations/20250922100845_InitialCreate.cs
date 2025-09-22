315d4d428f5fe31e9d7aaa58586b6a6cce002488
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-09-22 12:40:40 +0200

    fix: public classes

--- src/Arius.Core/Shared/StateRepositories/Migrations/20250922100845_InitialCreate.cs
+++ src/Arius.Core/Shared/StateRepositories/Migrations/20250922100845_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Arius.Core.Shared.StateRepositories.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    internal partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
