c2f2fe8778131f617357f5505f02ed5b487de720
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-10-01 14:38:53 +0200

    feat: refactor common CliCommand code into ProgressCliCommand

--- src/Arius.Cli/CliCommands/ArchiveCliCommand.cs
+++ src/Arius.Cli/CliCommands/ArchiveCliCommand.cs
@@ -1,25 +1,17 @@
 ﻿using Arius.Core.Features.Commands.Archive;
 using Arius.Core.Shared.Storage;
 using CliFx.Attributes;
-using CliFx.Exceptions;
-using CliFx.Infrastructure;
 using Mediator;
 using Microsoft.Extensions.Logging;
 using Spectre.Console;
 using System.Collections.Concurrent;
-using System.Threading.Channels;
 
 namespace Arius.Cli.CliCommands;
 
-public abstract class ArchiveCliCommandBase : CliFx.ICommand
+public abstract class ArchiveCliCommandBase : ProgressCliCommand<ArchiveCommand, Unit, ProgressUpdate>
 {
-    private readonly IMediator                      mediator;
-    private readonly ILogger<ArchiveCliCommandBase> logger;
-
-    public ArchiveCliCommandBase(IMediator mediator, ILogger<ArchiveCliCommandBase> logger)
+    protected ArchiveCliCommandBase(IMediator mediator, ILogger<ArchiveCliCommandBase> logger) : base(mediator, logger)
     {
-        this.mediator   = mediator;
-        this.logger = logger;
     }
 
     public abstract DirectoryInfo LocalRoot { get; init; }
@@ -32,7 +24,7 @@ public abstract class ArchiveCliCommandBase : CliFx.ICommand
 
     [CommandOption("container", 'c', IsRequired = true, Description = "Azure Blob Storage container name.")]
     public required string ContainerName { get; init; }
-    
+
     [CommandOption("passphrase", 'p', IsRequired = true, Description = "Passphrase for encryption.")]
     public required string Passphrase { get; init; }
 
@@ -42,104 +34,55 @@ public abstract class ArchiveCliCommandBase : CliFx.ICommand
     [CommandOption("remove-local", Description = "Remove local files after a successful upload.")]
     public bool RemoveLocal { get; init; } = false;
 
-
-    public async ValueTask ExecuteAsync(IConsole console)
+    protected override ArchiveCommand CreateCommand(IProgress<ProgressUpdate> progressReporter)
     {
-        try
+        return new ArchiveCommand
         {
-            AnsiConsole.Write(
-                new FigletText("Arius")
-                    .LeftJustified()
-                    .Color(Color.Red));
+            AccountName      = AccountName,
+            AccountKey       = AccountKey,
+            ContainerName    = ContainerName,
+            Passphrase       = Passphrase,
+            RemoveLocal      = RemoveLocal,
+            Tier             = Tier,
+            LocalRoot        = LocalRoot,
+            ProgressReporter = progressReporter
+        };
+    }
 
-            await AnsiConsole.Progress()
-                .AutoRefresh(true)
-                .AutoClear(false)
-                .HideCompleted(true)
-                .Columns(
-                    new ElapsedTimeColumn(),
-                    new ProgressBarColumn(),
-                    new TaskDescriptionColumn { Alignment = Justify.Right }
-                )
-                .StartAsync(async ctx =>
-                {
-                    var progressUpdates = Channel.CreateUnbounded<ProgressUpdate>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = false, AllowSynchronousContinuations = false });
-
-                    // Create the Mediator command from the CLI arguments
-                    var command = new ArchiveCommand
-                    {
-                        AccountName      = AccountName,
-                        AccountKey       = AccountKey,
-                        ContainerName    = ContainerName,
-                        Passphrase       = Passphrase,
-                        RemoveLocal      = RemoveLocal,
-                        Tier             = Tier,
-                        LocalRoot        = LocalRoot,
-                        ProgressReporter = new Progress<ProgressUpdate>(u => progressUpdates.Writer.TryWrite(u))
-                    };
-
-                    // Send the command and start the progress display loop
-                    var cancellationToken = console.RegisterCancellationHandler();
-                    var commandTask       = mediator.Send(command, cancellationToken).AsTask();
-                    commandTask.ContinueWith(_ => progressUpdates.Writer.Complete());
-
-                    var taskDictionary = new ConcurrentDictionary<string, ProgressTask>();
-
-                    // Process progress updates as they arrive
-                    await foreach (var u in progressUpdates.Reader.ReadAllAsync(cancellationToken))
-                    {
-                        // Handle different types of progress updates
-                        if (u is TaskProgressUpdate tpu)
-                        {
-                            var isError = tpu.Percentage < 0;
-                            var color = isError ? "red" : "blue";
-                            var task = taskDictionary.GetOrAdd(tpu.TaskName, taskName => ctx.AddTask($"[{color}]{taskName}[/]").IsIndeterminate());
-                            if (!string.IsNullOrWhiteSpace(tpu.StatusMessage))
-                                task.Description = $"[{color}]{tpu.TaskName}[/] ({tpu.StatusMessage})";
-                            if (tpu.Percentage >= 100/* || isError*/)
-                                task.StopTask();
-                        }
-                        else if (u is FileProgressUpdate fpu)
-                        {
-                            var isError = fpu.Percentage < 0;
-                            var color = isError ? "red" : "blue";
-                            var task = taskDictionary.GetOrAdd(fpu.FileName, fileName => ctx.AddTask($"[{color}]{fileName}[/]"));
-                            task.Description = $"[{color}]{fpu.FileName.EscapeMarkup().TruncateAndRightJustify(50)}[/] ({fpu.StatusMessage?.TruncateAndLeftJustify(20)})";
-                            task.Value       = isError ? 0 : fpu.Percentage;
-                            if (fpu.Percentage >= 100/* || isError*/)
-                                task.StopTask();
-                        }
-                        else
-                        {
-                            AnsiConsole.MarkupLine($"[yellow]Unknown progress update type: {u.GetType().Name}[/]");
-                        }
-                    }
-
-                    await commandTask; // Propagate any exceptions from the command handler
-
-                    AnsiConsole.MarkupLine("[green]All files processed![/]");
-                });
-        }
-        catch (Exception e)
+    protected override void HandleProgressUpdate(ProgressUpdate update, ProgressContext ctx, ConcurrentDictionary<string, ProgressTask> taskDictionary)
+    {
+        if (update is TaskProgressUpdate tpu)
         {
-            logger.LogError(e, "Unhandled exception");
-            throw new CommandException(e.Message, showHelp: false, innerException: e);
+            var isError = tpu.Percentage < 0;
+            var color = isError ? "red" : "blue";
+            var task = taskDictionary.GetOrAdd(tpu.TaskName, taskName => ctx.AddTask($"[{color}]{taskName}[/]").IsIndeterminate());
+            if (!string.IsNullOrWhiteSpace(tpu.StatusMessage))
+                task.Description = $"[{color}]{tpu.TaskName}[/] ({tpu.StatusMessage})";
+            task.Value = isError ? 0 : tpu.Percentage;
+            if (tpu.Percentage >= 100)
+                task.StopTask();
         }
-        //catch (ValidationException e)
-        //{
-        //    throw new CommandException(e.Message, showHelp: true);
-        //}
-        //catch (Exception e)
-        //{
-        //    AnsiConsole.WriteException(e, ExceptionFormats.ShortenEverything);
-        //}
+        else if (update is FileProgressUpdate fpu)
+        {
+            var isError = fpu.Percentage < 0;
+            var color = isError ? "red" : "blue";
+            var task = taskDictionary.GetOrAdd(fpu.FileName, fileName => ctx.AddTask($"[{color}]{fileName}[/]"));
+            task.Description = $"[{color}]{fpu.FileName.EscapeMarkup().TruncateAndRightJustify(50)}[/] ({fpu.StatusMessage?.TruncateAndLeftJustify(20)})";
+            task.Value = isError ? 0 : fpu.Percentage;
+            if (fpu.Percentage >= 100)
+                task.StopTask();
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[yellow]Unknown progress update type: {update.GetType().Name}[/]");
+        }
     }
 }
 
 [Command("archive", Description = "Archives a local directory to Azure Blob Storage.")]
 public class ArchiveCliCommand: ArchiveCliCommandBase
 {
-    public ArchiveCliCommand(IMediator mediator, ILogger<ArchiveCliCommand> logger) : base(mediator, logger)
+    public ArchiveCliCommand(IMediator mediator, ILogger<ArchiveCliCommandBase> logger) : base(mediator, logger)
     {
     }
 
@@ -152,7 +95,7 @@ public class ArchiveCliCommand: ArchiveCliCommandBase
 [Command("archive", Description = "Archives a local directory to Azure Blob Storage. [Docker]")]
 public class ArchiveDockerCliCommand : ArchiveCliCommandBase
 {
-    public ArchiveDockerCliCommand(IMediator mediator, ILogger<ArchiveDockerCliCommand> logger) : base(mediator, logger)
+    public ArchiveDockerCliCommand(IMediator mediator, ILogger<ArchiveCliCommandBase> logger) : base(mediator, logger)
     {
     }
 
--- src/Arius.Cli/CliCommands/RestoreCliCommand.cs
+++ src/Arius.Cli/CliCommands/RestoreCliCommand.cs
@@ -1,24 +1,16 @@
 using Arius.Core.Features.Commands.Restore;
 using CliFx.Attributes;
-using CliFx.Exceptions;
-using CliFx.Infrastructure;
 using Mediator;
 using Microsoft.Extensions.Logging;
 using Spectre.Console;
 using System.Collections.Concurrent;
-using System.Threading.Channels;
 
 namespace Arius.Cli.CliCommands;
 
-public abstract class RestoreCliCommandBase : CliFx.ICommand
+public abstract class RestoreCliCommandBase : ProgressCliCommand<RestoreCommand, RestoreCommandResult, ProgressUpdate>
 {
-    private readonly IMediator                      mediator;
-    private readonly ILogger<RestoreCliCommandBase> logger;
-
-    public RestoreCliCommandBase(IMediator mediator, ILogger<RestoreCliCommandBase> logger)
+    protected RestoreCliCommandBase(IMediator mediator, ILogger<RestoreCliCommandBase> logger) : base(mediator, logger)
     {
-        this.mediator = mediator;
-        this.logger   = logger;
     }
 
     [CommandOption("accountname", 'n', IsRequired = true, Description = "Azure Storage Account name.", EnvironmentVariable = "ARIUS_ACCOUNT_NAME")]
@@ -44,88 +36,48 @@ public abstract class RestoreCliCommandBase : CliFx.ICommand
     [CommandOption("include-pointers", Description = "Create respective pointer files alongside the binaries.")]
     public bool IncludePointers { get; init; } = false;
 
-    public async ValueTask ExecuteAsync(IConsole console)
+    protected override RestoreCommand CreateCommand(IProgress<ProgressUpdate> progressReporter)
     {
-        try
+        return new RestoreCommand
         {
-            AnsiConsole.Write(
-                new FigletText("Arius")
-                    .LeftJustified()
-                    .Color(Color.Red));
+            AccountName      = AccountName,
+            AccountKey       = AccountKey,
+            ContainerName    = ContainerName,
+            Passphrase       = Passphrase,
+            LocalRoot        = LocalRoot,
+            Targets          = Targets,
+            Download         = Download,
+            IncludePointers  = IncludePointers,
+            ProgressReporter = progressReporter
+        };
+    }
 
-            await AnsiConsole.Progress()
-                .AutoRefresh(true)
-                .AutoClear(false)
-                .HideCompleted(true)
-                .Columns(
-                    new ElapsedTimeColumn(),
-                    new ProgressBarColumn(),
-                    new TaskDescriptionColumn { Alignment = Justify.Right })
-                .StartAsync(async ctx =>
-                {
-                    var progressUpdates = Channel.CreateUnbounded<ProgressUpdate>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = false, AllowSynchronousContinuations = false });
-
-                    // Create the Mediator command from the CLI arguments
-                    var command = new RestoreCommand
-                    {
-                        AccountName     = AccountName,
-                        AccountKey      = AccountKey,
-                        ContainerName   = ContainerName,
-                        Passphrase      = Passphrase,
-                        LocalRoot       = LocalRoot,
-                        Targets         = Targets,
-                        Download        = Download,
-                        IncludePointers = IncludePointers,
-                        ProgressReporter = new Progress<ProgressUpdate>(u => progressUpdates.Writer.TryWrite(u))
-                    };
-
-                    // Start the restore command in the background
-                    var cancellationToken = console.RegisterCancellationHandler();
-                    var commandTask       = mediator.Send(command, cancellationToken).AsTask();
-                    commandTask.ContinueWith(_ => progressUpdates.Writer.Complete());
-
-                    var taskDictionary = new ConcurrentDictionary<string, ProgressTask>();
-
-                    // Process progress updates as they arrive
-                    await foreach (var u in progressUpdates.Reader.ReadAllAsync(cancellationToken))
-                    {
-                        // Handle different types of progress updates
-                        if (u is TaskProgressUpdate tpu)
-                        {
-                            var isError = tpu.Percentage < 0;
-                            var color = isError ? "red" : "cyan1";
-                            var task = taskDictionary.GetOrAdd(tpu.TaskName, taskName => ctx.AddTask($"[{color}]{taskName}[/]").IsIndeterminate());
-                            if (!string.IsNullOrWhiteSpace(tpu.StatusMessage))
-                                task.Description = $"[{color}]{tpu.TaskName}[/] ({tpu.StatusMessage})";
-                            task.Value = isError ? 0 : tpu.Percentage;
-                            if (tpu.Percentage >= 100/* || isError*/)
-                                task.StopTask();
-                        }
-                        else if (u is FileProgressUpdate fpu)
-                        {
-                            var isError = fpu.Percentage < 0;
-                            var color = isError ? "red" : "cyan3";
-                            var task = taskDictionary.GetOrAdd(fpu.FileName, fileName => ctx.AddTask($"[{color}]{fileName}[/]"));
-                            task.Description = $"[{color}]{fpu.FileName.EscapeMarkup().TruncateAndRightJustify(50)}[/] ({fpu.StatusMessage?.TruncateAndLeftJustify(20)})";
-                            task.Value       = isError ? 0 : fpu.Percentage;
-                            if (fpu.Percentage >= 100/* || isError*/)
-                                task.StopTask();
-                        }
-                        else
-                        {
-                            AnsiConsole.MarkupLine($"[yellow]Unknown progress update type: {u.GetType().Name}[/]");
-                        }
-                    }
-
-                    await commandTask; // Propagate any exceptions from the command handler
-
-                    AnsiConsole.MarkupLine("[green]All files processed![/]");
-                });
+    protected override void HandleProgressUpdate(ProgressUpdate update, ProgressContext ctx, ConcurrentDictionary<string, ProgressTask> taskDictionary)
+    {
+        if (update is TaskProgressUpdate tpu)
+        {
+            var isError = tpu.Percentage < 0;
+            var color = isError ? "red" : "cyan1";
+            var task = taskDictionary.GetOrAdd(tpu.TaskName, taskName => ctx.AddTask($"[{color}]{taskName}[/]").IsIndeterminate());
+            if (!string.IsNullOrWhiteSpace(tpu.StatusMessage))
+                task.Description = $"[{color}]{tpu.TaskName}[/] ({tpu.StatusMessage})";
+            task.Value = isError ? 0 : tpu.Percentage;
+            if (tpu.Percentage >= 100)
+                task.StopTask();
         }
-        catch (Exception e)
+        else if (update is FileProgressUpdate fpu)
         {
-            logger.LogError(e, "Unhandled exception");
-            throw new CommandException(e.Message, showHelp: false, innerException: e);
+            var isError = fpu.Percentage < 0;
+            var color = isError ? "red" : "cyan3";
+            var task = taskDictionary.GetOrAdd(fpu.FileName, fileName => ctx.AddTask($"[{color}]{fileName}[/]"));
+            task.Description = $"[{color}]{fpu.FileName.EscapeMarkup().TruncateAndRightJustify(50)}[/] ({fpu.StatusMessage?.TruncateAndLeftJustify(20)})";
+            task.Value = isError ? 0 : fpu.Percentage;
+            if (fpu.Percentage >= 100)
+                task.StopTask();
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[yellow]Unknown progress update type: {update.GetType().Name}[/]");
         }
     }
 }
@@ -133,7 +85,7 @@ public abstract class RestoreCliCommandBase : CliFx.ICommand
 [Command("restore", Description = "Restores a directory from Azure Blob Storage.")]
 public class RestoreCliCommand : RestoreCliCommandBase
 {
-    public RestoreCliCommand(IMediator mediator, ILogger<RestoreCliCommand> logger) : base(mediator, logger)
+    public RestoreCliCommand(IMediator mediator, ILogger<RestoreCliCommandBase> logger) : base(mediator, logger)
     {
     }
 
@@ -146,7 +98,7 @@ public class RestoreCliCommand : RestoreCliCommandBase
 [Command("restore", Description = "Restores a directory from Azure Blob Storage. [Docker]")]
 public class RestoreDockerCliCommand : RestoreCliCommandBase
 {
-    public RestoreDockerCliCommand(IMediator mediator, ILogger<RestoreDockerCliCommand> logger) : base(mediator, logger)
+    public RestoreDockerCliCommand(IMediator mediator, ILogger<RestoreCliCommandBase> logger) : base(mediator, logger)
     {
     }
 
