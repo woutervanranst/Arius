d5eb7c1452deb022719444bb2adc792fd5d102a4
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-10-22 21:02:09 +0200

    chore: remove unused test

--- src/Arius.Core.Tests/Features/Commands/Archive/ArchiveCommandHandlerUploadIfNotExistsTests.cs
+++ src/Arius.Core.Tests/Features/Commands/Archive/ArchiveCommandHandlerUploadIfNotExistsTests.cs
@@ -27,24 +27,6 @@ public class ArchiveCommandHandlerUploadIfNotExistsTests : IClassFixture<Fixture
     }
 
 
-    //[Fact]
-    //[Trait("Category", "SkipCI")]
-    //public async Task RunArchiveCommandTEMP() // NOTE TEMP this one is skipped in CI via the SkipCI category
-    //{
-    //    var logger = new FakeLogger<ArchiveCommandHandler>();
-
-    //    // TODO Make this better
-    //    var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-    //    var c = new ArchiveCommandBuilder(fixture)
-    //        .WithLocalRoot(isWindows ? 
-    //            new DirectoryInfo("C:\\Users\\WouterVanRanst\\Downloads\\Photos-001 (1)") : 
-    //            new DirectoryInfo("/mnt/c/Users/WouterVanRanst/Downloads/Photos-001 (1)"))
-    //        .Build();
-    //    await handler.Handle(c, CancellationToken.None);
-    //}
-
-
-
     [Fact]
     public async Task UploadIfNotExistsAsync_WhenChunkDoesNotExist_ShouldUpload()
     {
