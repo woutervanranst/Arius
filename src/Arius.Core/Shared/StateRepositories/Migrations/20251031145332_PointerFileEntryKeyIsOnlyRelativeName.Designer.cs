f207d00b867a632cb95635d5d0d8639f16feec17
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-10-31 16:08:01 +0100

    feat: update DbContext.PointerFileEntries to have only RelativeName as key

--- src/Arius.Core/Shared/StateRepositories/Migrations/20251031145332_PointerFileEntryKeyIsOnlyRelativeName.Designer.cs
+++ src/Arius.Core/Shared/StateRepositories/Migrations/20251031145332_PointerFileEntryKeyIsOnlyRelativeName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Arius.Core.Shared.StateRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Arius.Core.Shared.StateRepositories.Migrations
 {
     [DbContext(typeof(StateRepositoryDbContext))]
-    partial class StateRepositoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251031145332_PointerFileEntryKeyIsOnlyRelativeName")]
+    partial class PointerFileEntryKeyIsOnlyRelativeName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
---
+++ src/Arius.Core/Shared/StateRepositories/Migrations/20251031145332_PointerFileEntryKeyIsOnlyRelativeName.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arius.Core.Shared.StateRepositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class PointerFileEntryKeyIsOnlyRelativeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PointerFileEntries",
+                table: "PointerFileEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PointerFileEntries_RelativeName",
+                table: "PointerFileEntries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PointerFileEntries",
+                table: "PointerFileEntries",
+                column: "RelativeName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PointerFileEntries",
+                table: "PointerFileEntries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PointerFileEntries",
+                table: "PointerFileEntries",
+                columns: new[] { "Hash", "RelativeName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PointerFileEntries_RelativeName",
+                table: "PointerFileEntries",
+                column: "RelativeName");
+        }
+    }
+}
