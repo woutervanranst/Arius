ae8e14087fa3664a2c16d49c8fa6118f0f19a290
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-11-12 13:57:32 +0100

    chore: change visiblity

--- src/Arius.Core/Shared/StateRepositories/Migrations/20251031145332_PointerFileEntryKeyIsOnlyRelativeName.cs
+++ src/Arius.Core/Shared/StateRepositories/Migrations/20251031145332_PointerFileEntryKeyIsOnlyRelativeName.cs
@@ -5,7 +5,7 @@
 namespace Arius.Core.Shared.StateRepositories.Migrations
 {
     /// <inheritdoc />
-    public partial class PointerFileEntryKeyIsOnlyRelativeName : Migration
+    internal partial class PointerFileEntryKeyIsOnlyRelativeName : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
