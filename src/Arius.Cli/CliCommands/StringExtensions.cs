15fa1557267c5f04cd5d3fe9a167550379b67057
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-11-26 17:52:30 +0100

    chore: migrate to extension blocks

--- src/Arius.Cli/CliCommands/StringExtensions.cs
+++ src/Arius.Cli/CliCommands/StringExtensions.cs
@@ -2,20 +2,23 @@
 
 internal static class StringExtensions
 {
-    public static string TruncateAndRightJustify(this string input, int width)
+    extension(string input)
     {
-        if (width <= 0) return string.Empty;
-        const string ellipsis     = "...";
-        var          contentWidth = width - ellipsis.Length;
-        if (contentWidth <= 0) return ellipsis[..width];
-        string truncated = input.Length > contentWidth ? ellipsis + input[^contentWidth..] : input;
-        return truncated.PadLeft(width);
-    }
+        public string TruncateAndRightJustify(int width)
+        {
+            if (width <= 0) return string.Empty;
+            const string ellipsis     = "...";
+            var          contentWidth = width - ellipsis.Length;
+            if (contentWidth <= 0) return ellipsis[..width];
+            string truncated = input.Length > contentWidth ? ellipsis + input[^contentWidth..] : input;
+            return truncated.PadLeft(width);
+        }
 
-    public static string TruncateAndLeftJustify(this string input, int width)
-    {
-        if (width <= 0) return string.Empty;
-        var truncated = input.Length > width ? input[..width] : input;
-        return truncated.PadRight(width);
+        public string TruncateAndLeftJustify(int width)
+        {
+            if (width <= 0) return string.Empty;
+            var truncated = input.Length > width ? input[..width] : input;
+            return truncated.PadRight(width);
+        }
     }
 }
